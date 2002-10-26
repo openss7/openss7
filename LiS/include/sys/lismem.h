3e6a91113a4aea54ab5aaa64f22c96d604fb85fb
Brian Bidulock <bidulock@openss7.org>
2002-10-25 21:52:34 -0600

    Import from CVS Fri Oct 25 21:52:34 2002 -0600
    Import of LiS-2.16.0
    Initial revision

---
+++ LiS/include/sys/lismem.h
@@ -0,0 +1,73 @@
+/************************************************************************
+*                        LiS Memory Interface                           *
+*************************************************************************
+*									*
+* This is an interface to the kernel's memory allocation routines.	*
+* The idea is that STREAMS drivers that use these routines to allocate	*
+* memory can be insulated in changes to the parameters in the kernel's	*
+* allocation routines.  These parameters DID change from the 2.2 kernel	*
+* to the 2.4 kernel.							*
+*									*
+*    Copyright (C) 2000  David Grothe, Gcom, Inc <dave@gcom.com>	*
+*									*
+* This library is free software; you can redistribute it and/or		*
+* modify it under the terms of the GNU Library General Public		*
+* License as published by the Free Software Foundation; either		*
+* version 2 of the License, or (at your option) any later version.	*
+* 									*
+* This library is distributed in the hope that it will be useful,	*
+* but WITHOUT ANY WARRANTY; without even the implied warranty of	*
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU	*
+* Library General Public License for more details.			*
+* 									*
+* You should have received a copy of the GNU Library General Public	*
+* License along with this library; if not, write to the			*
+* Free Software Foundation, Inc., 59 Temple Place - Suite 330,		*
+* Cambridge, MA 02139, USA.						*
+*									*
+************************************************************************/
+
+#ifndef SYS_LISMEM_H
+#define SYS_LISMEM_H	1
+
+#ident "@(#) LiS lismem.h 1.3 10/25/02"
+
+/*
+ * These are the functions that to the work.  Do not call them
+ * directly unless you are cascading file and line number information
+ * from your own interface routines.
+ *
+ * Note that lis_free_mem returns a void *.  This is always a NULL pointer
+ * value for the convenience of constructs such as ptr = lis_free_mem(ptr).
+ * Also, the mem_area argument to lis_free_mem can be NULL.
+ */
+void	*lis_alloc_atomic_fcn(int nbytes, char *file, int line) ;
+void	*lis_alloc_kernel_fcn(int nbytes, char *file, int line) ;
+void	*lis_alloc_dma_fcn(int nbytes, char *file, int line) ;
+void	*lis_free_mem_fcn(void *mem_area, char *file, int line) ;
+void	*lis_get_free_pages_fcn(int nbytes, int class, char *file, int line) ;
+void	*lis_get_free_pages_atomic_fcn(int nbytes, char *file, int line) ;
+void	*lis_get_free_pages_kernel_fcn(int nbytes, char *file, int line) ;
+void	*lis_free_pages_fcn(void *ptr, char *file, int line) ;
+void	 lis_free_all_pages(void) ;	/* internal cleanup routine */
+
+/*
+ * These are the simplified routines that the STREAMS drivers call.
+ * We avoid using the values of the kernel's memory class parameters
+ * at the calling site so that only LiS needs to be compiled in the
+ * context of the new kernel.
+ */
+#define	lis_alloc_atomic(n)	lis_alloc_atomic_fcn((n), __FILE__, __LINE__)
+#define	lis_alloc_kernel(n)	lis_alloc_kernel_fcn((n), __FILE__, __LINE__)
+#define	lis_alloc_dma(n)	lis_alloc_dma_fcn((n), __FILE__, __LINE__)
+#define	lis_free_mem(p)		lis_free_mem_fcn((p), __FILE__, __LINE__)
+#define	lis_get_free_pages(n)	lis_get_free_pages_kernel_fcn((n), \
+							__FILE__, __LINE__)
+#define	lis_get_free_pages_kernel(n)	lis_get_free_pages_kernel_fcn((n), \
+							__FILE__, __LINE__)
+#define	lis_get_free_pages_atomic(n)	lis_get_free_pages_atomic_fcn((n), \
+							__FILE__, __LINE__)
+#define	lis_free_pages(p)	lis_free_pages_fcn((p), __FILE__, __LINE__)
+
+
+#endif				/* SYS_LISPCI_H from top of file */
---
+++ LiS/include/sys/strport.h
@@ -0,0 +1,104 @@
+/*                               -*- Mode: C -*- 
+ * <strport.h> --- Linux STREAMS portability declarations. 
+ * Author          : gram & nemo
+ * Created On      : Fri Mar 24 2:40:21 1995
+ * RCS Id          ; $Id: strport.h,v 1.4 1996/01/20 17:00:53 dave Exp $
+ * Last Modified By: David Grothe
+ * Restrictions    : SHAREd items can be read/writen by usr
+ *                 : EXPORTed items can only be read by usr
+ *                 : PRIVATEd items cannot be read nor writen
+ * Purpose         : All system dependent stuff goes here. The idea
+ *                 : is that different versions of this file can be
+ *                 : used to port STREAMS to other operating systems
+ *                 : as well as providing a user-space testbed environment.
+ *
+ *    Copyright (C) 1995  Graham Wheeler, Francisco J. Ballesteros
+ *    Copyright (C) 1997  David Grothe, Gcom, Inc <dave@gcom.com>
+ *
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330, Cambridge,
+ * MA 02139, USA.
+ * 
+ *
+ *    You can reach us by email to any of
+ *    gram@aztec.co.za, nemo@ordago.uc3m.es
+ */
+
+#ifndef _STRPORT_H
+#define _STRPORT_H
+
+#ident "@(#) LiS strport.h 2.2 10/25/02 21:32:03 "
+
+/*  *******************************************************************  */
+/*                               Dependencies                            */
+
+#if	defined( __MSDOS__)
+#include <sys/LiS/dos-mdep.h>
+#elif defined(LINUX)
+#include <sys/LiS/linux-mdep.h>
+#elif defined(USER)
+#include <sys/LiS/user-mdep.h>
+#elif defined (QNX)
+#include <sys/LiS/qnx-mdep.h>
+#elif defined(SYS_SCO)
+#include <sys/LiS/sco-mdep.h>
+#elif defined(SYS_54)
+#include <sys/LiS/sys54-mdep.h>
+#elif defined(PORTABLE)
+#include <sys/LiS/port-mdep.h>
+#endif /* !__MSDOS__ */
+
+/*
+ * Establish some version dependent sub-defines for all to see.
+ */
+/*
+ * If neither KERNEL_2_0 nor KERNEL_2_1 is defined then we are
+ * being compiled as portable code.  We want to use portable constructs
+ * for such things as poll.
+ */
+#if !defined(KERNEL_2_0) && !defined(KERNEL_2_1)
+#define PORTABLE_POLL   1
+#else
+# if defined(KERNEL_2_0)
+# define PORTABLE_POLL   1
+# elif defined(KERNEL_2_1)
+# define LINUX_POLL      2
+# else
+# error "ifdef logic error involving KERNEL_2_0 and KERNEL_2_1"
+# endif
+#endif
+
+#ifndef OPENFAIL
+#define OPENFAIL	(-1)
+#endif
+#ifndef INFPSZ
+#define INFPSZ		(-1)
+#endif
+
+#ifdef __KERNEL__
+extern char	*lis_errmsg(int lvl) ;
+extern void	*lis_malloc(int nbytes, int class, int use_cache,
+					char *file_name,int line_nr);
+extern void	 lis_free(void *ptr, char *file_name,int line_nr);
+#endif				/* __KERNEL__ */
+
+#endif /* _STRPORT_H */
+
+
+/*----------------------------------------------------------------------
+# Local Variables:      ***
+# change-log-default-name: "~/src/prj/streams/src/NOTES" ***
+# End: ***
+  ----------------------------------------------------------------------*/
