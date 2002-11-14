db78f534e9a5d785a3a4a10f3469b144a6c978a6
Brian Bidulock <bidulock@openss7.org>
2002-11-14 15:30:32 -0600

    Import from CVS Thu Nov 14 15:30:32 2002 -0600
    Import of LiS-2.16.0
    Initial revision

---
+++ LiS/head/linux/lismem.c
@@ -0,0 +1,480 @@
+/************************************************************************
+*                        LiS Memory Interface                           *
+*************************************************************************
+*									*
+* This interface was created to insulate the Linux STREAMS programmer	*
+* from the constantly changing memory allocation interface provided by	*
+* the Linux kernel.							*
+*									*
+*    Copyright (C) 2000  David Grothe, Gcom, Inc <dave@gcom.com>	*
+*									*
+* This library is free software; you can redistribute it and/or		*
+* modify it under the terms of the GNU Library General Public		*
+* License as published by the Free Software Foundation; either		*
+* version 2 of the License, or (at your option) any later version.	*
+* 									*
+* This library is distributed in the hope that it will be useful,	*
+* but WITHOUT ANY WARRANTY; without even the implied warranty of	*
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU	*
+* Library General Public License for more details.			*
+* 									*
+* You should have received a copy of the GNU Library General Public	*
+* License along with this library; if not, write to the			*
+* Free Software Foundation, Inc., 59 Temple Place - Suite 330,		*
+* Cambridge, MA 02139, USA.						*
+*									*
+************************************************************************/
+
+#ident "@(#) LiS lismem.c 1.14 11/14/02"
+
+#include <sys/stream.h>		/* gets all the right LiS stuff included */
+#include <sys/lismem.h>		/* LiS mem header file */
+#include <sys/osif.h>		/* LiS kernel interface */
+#include <sys/lislocks.h>	/* for spin locks */
+
+#ifdef POISON_MEM
+#define CACHE_OPTS	(SLAB_POISON | SLAB_RED_ZONE)
+#else
+#define CACHE_OPTS	0
+#endif
+
+/************************************************************************
+*                         Cache Allocation                              *
+*************************************************************************
+*									*
+* These sizes are chosen to accomodate stream head structures, mblks	*
+* and queue structures.							*
+*									*
+************************************************************************/
+
+#if defined(KERNEL_2_3)
+
+typedef struct
+{
+    const char		 *name ;
+    kmem_cache_t	 *cache_struct ;
+    int			  size ;
+    int			  kflags ;
+
+} lis_slab_table_t ;
+
+#define	SIZE1		200		/* numeric form */
+#define	SIZE2		512
+#define SIZE3		1500
+#define SIZE_MAX	SIZE3
+#define	SIZE1S		"200"		/* string form */
+#define	SIZE2S		"512"
+#define SIZE3S		"1500"
+
+lis_slab_table_t lis_slab_table[] =
+{
+    {"LiS-Atomic-"SIZE1S, NULL, SIZE1, GFP_ATOMIC},
+    {"LiS-Atomic-"SIZE2S, NULL, SIZE2, GFP_ATOMIC},
+    {"LiS-Atomic-"SIZE3S, NULL, SIZE3, GFP_ATOMIC},
+
+    {"LiS-Kernel-"SIZE1S, NULL, SIZE1, GFP_KERNEL},
+    {"LiS-Kernel-"SIZE2S, NULL, SIZE2, GFP_KERNEL},
+    {"LiS-Kernel-"SIZE3S, NULL, SIZE3, GFP_KERNEL},
+
+    {"LiS-DMA-"SIZE1S,    NULL, SIZE1, GFP_DMA},
+    {"LiS-DMA-"SIZE2S,    NULL, SIZE2, GFP_DMA},
+    {"LiS-DMA-"SIZE3S,    NULL, SIZE3, GFP_DMA},
+
+    {NULL,                NULL, 0,     0}
+} ;
+
+#endif
+
+/************************************************************************
+*                         lismem_init                                   *
+*************************************************************************
+*									*
+* Self-initialization for memory functions.				*
+*									*
+************************************************************************/
+
+#define chk_init	if (!lismem_init_flag) lis_mem_init()
+
+static int  		lismem_init_flag ;
+static lis_spin_lock_t	contig_lock ;		/* protect the table */
+
+void lis_mem_init(void)
+{
+    lis_spin_lock_init(&contig_lock, "LiS-Page-Alloc");
+
+#if defined(KERNEL_2_3)
+    {
+	lis_slab_table_t	*p ;
+
+	for (p = lis_slab_table; p->name != NULL; p++)
+	{
+	    p->cache_struct = kmem_cache_create(p->name, p->size, 0,
+				SLAB_HWCACHE_ALIGN | CACHE_OPTS, NULL,NULL);
+	}
+    }
+#endif
+
+    lismem_init_flag = 1 ;
+}
+
+void lis_mem_terminate(void)
+{
+#if defined(KERNEL_2_3)
+    lis_slab_table_t	*p ;
+
+    for (p = lis_slab_table; p->name != NULL; p++)
+    {
+	if (p->cache_struct == NULL) continue ;
+	kmem_cache_destroy(p->cache_struct) ;
+	p->cache_struct = NULL ;
+    }
+#endif
+}
+
+/************************************************************************
+*                          Page Allocation                              *
+*************************************************************************
+*									*
+* Page allocation is the way to ensure that you get memory that		*
+* occupies physically contiguous locations.  We offer a simplified	*
+* interface to the kernel's page allocator here.  The kernel's page	*
+* allocation routines are awkward to use in that you have to pass	*
+* the number of pages to de-allocate to their "free pages" routine.	*
+*									*
+* We keep a separate table of allocated pages.  This allows us to return*
+* the entire page to the caller.					*
+*									*
+************************************************************************/
+
+typedef struct contig_memlink
+{				/* 32 bytes long */
+    struct contig_memlink *link;/* to next entry */
+    void	*page_addr ;	/* addr of allocated page */
+    int		 size ;		/* original size in bytes */
+    int		 order ;	/* "order" param for freeing */
+    char	*file ;		/* who allocated */
+    int		 line ;
+    int		 boundary ;	/* page boundary */
+
+} contig_memlink_t ;
+
+static contig_memlink_t	*contig_mem_head ;
+static unsigned long	contig_bytes ;		/* total allocated */
+
+static int	alloc_page_tbl(void)
+{
+    contig_memlink_t	*pg ;
+    contig_memlink_t	*p ;
+    int			 nentries ;
+
+    pg = (contig_memlink_t *) __get_free_page(GFP_KERNEL) ;
+    if (pg == NULL) 
+	return(-1) ;
+
+    memset(pg, 0, PAGE_SIZE) ;
+    for (p = pg, nentries = PAGE_SIZE/sizeof(*p); nentries > 0; nentries--, p++)
+    {
+	p->link = p + 1 ;
+    }
+
+    /*
+     * Link the last entry in this page to the head entry of the previous page.
+     * Then make this page the new head of the list.
+     */
+    pg->boundary = 1 ;			/* 1st entry is on page boundary */
+    lis_spin_lock(&contig_lock) ;
+    (p-1)->link = contig_mem_head ;	/* last entry in page */
+    contig_mem_head = pg ;
+    lis_spin_unlock(&contig_lock) ;
+    return(0) ;
+}
+
+static contig_memlink_t *find_entry(void *addr)
+{
+    contig_memlink_t	*mp ;
+
+    for (mp = contig_mem_head; mp != NULL; mp = mp->link)
+    {
+	if (mp->page_addr == addr) return(mp) ;
+    }
+
+    return(NULL) ;			/* could not find it */
+}
+
+void    *lis_get_free_pages_fcn(int nbytes, int class, char *file, int line)
+{
+    void		*a ;
+    int			 order ;
+    int			 bytes ;
+    contig_memlink_t	*mp ;
+
+    chk_init ;
+
+    for (order = 0; (bytes = (1 << order) * PAGE_SIZE) < nbytes; order++) ;
+
+    lis_spin_lock(&contig_lock) ;
+    mp = find_entry(NULL) ;		/* find available entry */
+    if (mp == NULL)
+    {
+	lis_spin_unlock(&contig_lock) ;
+	if (alloc_page_tbl() < 0)
+	    return(NULL) ;		/* can't track allocated page */
+
+	lis_spin_lock(&contig_lock) ;
+	mp = find_entry(NULL) ;		/* should be one now */
+	if (mp == NULL)			/* busted code */
+	{
+	    lis_spin_unlock(&contig_lock) ;
+	    printk("lis_get_free_pages: called from %s #%d: "
+		    "bug in allocation table code\n",
+		    file, line) ;
+	    return(NULL) ;
+	}
+    }
+
+    mp->page_addr = (void *) 0xffffffff ;/* guard from find_entry */
+    lis_spin_unlock(&contig_lock) ;
+
+    a =  (void *) __get_free_pages(class, order) ;	/* kernel routine */
+    if (a == NULL)
+    {
+	printk("lis_get_free_pages: called from %s #%d: "
+		"nbytes=%d order=%d total=%lu allocation failed\n",
+		file, line,
+		nbytes, order, contig_bytes) ;
+	return(NULL) ;
+    }
+
+    contig_bytes += bytes ;		/* # bytes allocated */
+
+    mp->size         = nbytes ;
+    mp->order        = order ;
+    mp->file         = file ;
+    mp->line         = line ;
+    mp->page_addr    = a ;
+
+    return(a) ;
+
+} /* lis_get_free_pages_fcn */
+
+void    *lis_get_free_pages_atomic_fcn(int nbytes, char *file, int line)
+{
+    return(lis_get_free_pages_fcn(nbytes, GFP_ATOMIC, file, line)) ;
+}
+
+void    *lis_get_free_pages_kernel_fcn(int nbytes, char *file, int line)
+{
+    return(lis_get_free_pages_fcn(nbytes, GFP_KERNEL, file, line)) ;
+}
+
+void     *lis_free_pages_fcn(void *ptr, char *file, int line)
+{
+    contig_memlink_t	*mp ;
+    void		*addr ;
+    int			 order ;
+
+    chk_init ;
+
+    if (ptr == NULL)
+	return(NULL) ;
+
+    lis_spin_lock(&contig_lock) ;
+    mp = find_entry(ptr) ;
+    if (mp == NULL)			/* have no record of this page */
+    {
+	printk("lis_free_pages: called from %s #%d: "
+	       "cannot find page info for address 0x%lx\n",
+	       file, line, (long) ptr) ;
+	lis_spin_unlock(&contig_lock) ;
+	return(NULL) ;
+    }
+
+    addr = mp->page_addr ;
+    order = mp->order ;
+    contig_bytes -= (1 << mp->order) * PAGE_SIZE ;
+    mp->page_addr = NULL ;
+
+    lis_spin_unlock(&contig_lock) ;
+
+    free_pages((unsigned long) addr, order) ; /* kernel routine */
+
+    return(NULL) ;
+
+} /* lis_free_pages_fcn */
+
+void	lis_free_all_pages(void)
+{
+    contig_memlink_t	*mp ;
+    contig_memlink_t	*boundary_ptr = NULL ;
+    volatile static int  freeing ;
+
+    chk_init ;
+
+    lis_spin_lock(&contig_lock) ;
+    if (!freeing && contig_bytes != 0)
+    {
+	freeing = 1 ;			/* only one gets to do this */
+	lis_spin_unlock(&contig_lock) ;
+
+	/*
+	 * Free the data pages allocated to callers.
+	 */
+	for (mp = contig_mem_head; mp != NULL; mp = mp->link)
+	{
+	    if (mp->page_addr == NULL || mp->page_addr == (void *)0xffffffff)
+		continue ;
+
+	    if (LIS_DEBUG_MEM_LEAK)
+		printk("lis_free_all_pages: "
+			  "Memory at %lx allocated from %s #%d, "
+			  "%d bytes leaked (recovered)\n",
+			  (long) mp->page_addr, mp->file, mp->line, mp->size) ;
+	    lis_free_pages(mp->page_addr) ;
+	}
+
+	/*
+	 * Free the pages allocated for keeping track of other pages.
+	 */
+	for (mp = contig_mem_head; mp != NULL; mp = mp->link)
+	{
+	    if (!mp->boundary) continue ;
+
+	    /* At a page boundary, remember it for freeing */
+	    if (boundary_ptr != NULL)
+		free_page((unsigned long) boundary_ptr) ;
+
+	    boundary_ptr = mp ;		/* remember for next time */
+	}
+
+	if (boundary_ptr != NULL)	/* catch last page */
+	    free_page((unsigned long) boundary_ptr) ;
+
+	contig_mem_head = NULL ;
+	freeing = 0 ;
+    }
+    else
+	lis_spin_unlock(&contig_lock) ;
+
+} /* lis_free_all_pages */
+
+/************************************************************************
+*                         Kernel Allocators                             *
+*************************************************************************
+*									*
+* lis_kmalloc/lis_kfree are called from lis_malloc in order to actually	*
+* obtain the memory from the kernel.  We do this in either of two ways,	*
+* either from an lis memory cache or from the general kmalloc.		*
+*									*
+************************************************************************/
+#if defined(KERNEL_2_3)
+
+typedef struct
+{
+    kmem_cache_t	*slab_ptr ;
+
+} mem_hdr_t ;
+
+void *lis__kmalloc(int nbytes, int class, int use_cache)
+{
+    mem_hdr_t	  	*p ;
+    lis_slab_table_t	*tp ;
+    kmem_cache_t	*cp = NULL ;
+
+    nbytes += sizeof(mem_hdr_t) ;
+
+    if (!use_cache || nbytes > SIZE_MAX)
+	p = kmalloc(nbytes, class) ;
+    else
+    {				/* find appropriate slab */
+	for (tp = lis_slab_table; tp->name != NULL; tp++)
+	{
+	    if (   tp->kflags == class
+		&& tp->cache_struct != NULL
+		&& nbytes <= tp->size
+	       )
+		break ;
+	}
+
+	if (tp->cache_struct == NULL) return(NULL) ;
+
+	p = kmem_cache_alloc(cp = tp->cache_struct, class) ;
+    }
+
+    if (p == NULL) return(NULL) ;
+
+    p->slab_ptr = cp ;
+    return((void *)(p+1)) ;
+}
+
+void *lis__kfree(void *ptr)
+{
+    mem_hdr_t	 *p ;
+
+    if (ptr == NULL) return(NULL) ;
+
+    p = (mem_hdr_t *) ptr ;
+    p-- ;
+
+    if (p->slab_ptr == NULL)
+	kfree(p) ;
+    else
+	kmem_cache_free(p->slab_ptr, p) ;
+
+    return(NULL) ;
+}
+
+#else					/* 2.2 kernel */
+
+/*
+ * In the 2.2 kernel you can't actually free the slab allocator
+ * cache structures.  You can only "shrink" them.  This leads to
+ * leaving those structures allocated (somewhere) when LiS is
+ * unloaded from memory.  Sometimes they seem to have been allocated
+ * in memory that does not die with LiS, so that when LiS is loaded
+ * again we get "dup entry" messages when registering the caches.
+ *
+ * The technique here is to just not use the dedicated caches.
+ */
+void *lis__kmalloc(int nbytes, int class, int use_cache)
+{
+    return(kmalloc(nbytes, class)) ;
+}
+
+void *lis__kfree(void *ptr)
+{
+    kfree(ptr) ;
+    return(NULL) ;
+}
+
+#endif
+
+/************************************************************************
+*                       Calls to Kernel Routines                        *
+*************************************************************************
+*									*
+* We are really using the LiS memory allocator here so that we can	*
+* track file and line number information.				*
+*									*
+************************************************************************/
+
+void	*lis_alloc_atomic_fcn(int nbytes, char *file, int line)
+{
+    return(lis_malloc(nbytes, GFP_ATOMIC, 0, file, line)) ;
+}
+
+void	*lis_alloc_kernel_fcn(int nbytes, char *file, int line)
+{
+    return(lis_malloc(nbytes, GFP_KERNEL, 0, file, line)) ;
+}
+
+void	*lis_alloc_dma_fcn(int nbytes, char *file, int line)
+{
+    return(lis_malloc(nbytes, GFP_DMA, 0, file, line)) ;
+}
+
+void	*lis_free_mem_fcn(void *mem_area, char *file, int line)
+{
+    lis_free(mem_area, file, line) ;
+    return(NULL) ;
+}
+
---
+++ LiS/util/streams.c
@@ -0,0 +1,497 @@
+/*
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330, Cambridge,
+ * MA 02139, USA.
+ * 
+ */
+#ident "@(#) LiS streams.c 2.12 11/14/02 15:30:32 "
+#include <sys/types.h>
+#undef GCOM_OPEN
+#include <sys/stropts.h>
+#include <sys/LiS/stats.h>
+#ifdef QNX
+#include <unix.h>
+#define	LOOP_CLONE	"/dev/gcom/loop_clone"
+#else
+#define	LOOP_CLONE	"/dev/loop_clone"
+#endif
+#include <sys/ioctl.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/*
+ * Storage Declarations
+ */
+unsigned long strstats[STRMAXSTAT][4]; /* the stats */
+
+char		*prog_name ;
+
+int		cflag = 0;
+int		Cflag = 0;
+int		mflag = 0;
+int		dflag = 0;
+int		Dflag = 0;
+int		sflag = 0;
+int		Sflag = 0;
+int		tflag = 0;
+int		pflag = 0;
+int		qflag = 0;
+int		Hflag = 0;
+
+long		memK = 0 ;
+long		msgmemK = 0 ;
+
+lis_qrun_stats_t	qrun_stats ;
+
+/*
+ * Names of stat slots
+ */
+itemname_t lis_itemnames[] =
+{
+    {MODUSE,		MODUSESTR},
+    {MEMALLOCS,		MEMALLOCSSTR},
+    {MEMFREES,		MEMFREESSSTR},
+    {MEMALLOCD,		MEMALLOCDSTR},
+    {MEMLIM,		MEMLIMSTR},
+    {HEADERS,		HEADERSSTR},
+    {FREEHDRS,		FREEHDRSSTR},
+    {DATABS,		DATABSSTR},
+    {DBLKMEM,		DBLKMEMSTR},
+    {MSGMEMLIM,		MSGMEMLIMSTR},
+    {BUFCALLS,		BUFCALLSSTR},
+    {MSGSQD,		MSGSQDSTR},
+    {MSGQDSTRHD,	MSGQDSTRHDSTR},
+    {CANPUTS,		CANPUTSSTR},
+    {QUEUES,		QUEUESSTR},
+    {QSCHEDS,		QSCHEDSSTR},
+    {OPENTIME,		OPENTIMESTR},
+    {CLOSETIME,		CLOSETIMESTR},
+    {READTIME,		READTIMESTR},
+    {WRITETIME,		WRITETIMESTR},
+    {IOCTLTIME,		IOCTLTIMESTR},
+    {GETMSGTIME,	GETMSGTIMESTR},
+    {PUTMSGTIME,	PUTMSGTIMESTR},
+    {POLLTIME,		POLLTIMESTR}
+};
+
+char *lis_countnames[] =
+{
+    CURRENTSTR, TOTALSTR, MAXIMUMSTR, FAILURESSTR
+};
+
+void
+LisShowStrStats(void)
+{
+    int i;
+    int inx;
+    int found_time = 0 ;
+    int have_hdr = 0 ;
+
+    if (!sflag && !tflag) return ;	/* no stats desired */
+
+    /* Print statistics */
+    for (i = 0; i < STRMAXSTAT; i++)
+    {
+	int j;
+
+	if (lis_itemnames[i].name == NULL) continue ;
+
+	inx = lis_itemnames[i].stats_inx ;
+	if (inx == OPENTIME)
+	{				/* time for a new header */
+	    found_time = 1 ;
+	    have_hdr = 0 ;
+	}
+
+	if (found_time)			/* got to time stats */
+	{
+	    if (!tflag) break ;		/* not supposed to print them */
+	}
+	else				/* still in regular stats */
+	if (!sflag)			/* not supposed to print them */
+	    continue ;
+
+	if (!have_hdr)
+	{
+	    int	k ;
+	    have_hdr = 1 ;
+	    printf("\n%-28s  ", "");
+	    for (k = 0; k < 4; k++)
+		printf("%12s", lis_countnames[k]) ;
+	    printf("\n");
+	}
+
+	printf("%-28s: ", lis_itemnames[i].name);
+	for (j = 0; j < 4; j++)
+	    printf("%12lu", strstats[inx][j]);
+	printf("\n");
+    }
+
+
+} /*LisShowStrStats*/
+
+void print_qrun_stats(void)
+{
+    int		cpu ;
+
+    printf("N-CPUs N-Qrunners N-Running N-Requested\n"
+	   "%6u %10u %9u %10u\n",
+	    qrun_stats.num_cpus, qrun_stats.num_qrunners,
+	    qrun_stats.queues_running, qrun_stats.runq_req_cnt) ;
+
+    printf(
+      "\nCPU   Qsched-Cnts Qsched-ISR Svc-Q-Cnts Qrun-Cnts Active Thread-PID\n"
+          ) ;
+    for (cpu = 0; cpu < qrun_stats.num_cpus; cpu++)
+    {
+	printf("%3u   %11lu %10lu %10lu %9lu %6u %10u\n",
+		cpu,
+		qrun_stats.setqsched_cnts[cpu],
+		qrun_stats.setqsched_isr_cnts[cpu],
+		qrun_stats.runq_cnts[cpu], qrun_stats.queuerun_cnts[cpu],
+		qrun_stats.active_flags[cpu], qrun_stats.runq_pids[cpu]) ;
+    }
+}
+
+
+void print_debug_bits(void)
+{
+    printf("-d<debug-bits -- \n") ;
+    printf("DEBUG_OPEN          0x00000001\n");
+    printf("DEBUG_CLOSE         0x00000002\n");
+    printf("DEBUG_READ          0x00000004\n");
+    printf("DEBUG_WRITE         0x00000008\n");
+    printf("DEBUG_IOCTL         0x00000010\n");
+    printf("DEBUG_PUTNEXT       0x00000020\n");
+    printf("DEBUG_STRRPUT       0x00000040\n");
+    printf("DEBUG_SIG           0x00000080\n");
+    printf("DEBUG_PUTMSG        0x00000100\n");
+    printf("DEBUG_GETMSG        0x00000200\n");
+    printf("DEBUG_POLL          0x00000400\n");
+    printf("DEBUG_LINK          0x00000800\n");
+    printf("DEBUG_MEAS_TIME     0x00001000\n");
+    printf("DEBUG_MEM_LEAK      0x00002000\n");
+    printf("DEBUG_FLUSH         0x00004000\n");
+    printf("DEBUG_FATTACH       0x00008000\n");
+    printf("DEBUG_SAFE          0x00010000\n");
+    printf("DEBUG_TRCE_MSG      0x00020000\n");
+    printf("DEBUG_CLEAN_MSG     0x00040000\n");
+    printf("DEBUG_SPL_TRACE     0x00080000\n");
+    printf("DEBUG_MP_ALLOC      0x00100000\n");
+    printf("DEBUG_MP_FREEMSG    0x00200000\n");
+    printf("DEBUG_MALLOC        0x00400000\n");
+    printf("DEBUG_MONITOR_MEM   0x00800000\n");
+    printf("DEBUG_DMP_QUEUE     0x01000000\n");
+    printf("DEBUG_DMP_MBLK      0x02000000\n");
+    printf("DEBUG_DMP_DBLK      0x04000000\n");
+    printf("DEBUG_DMP_STRHD     0x08000000\n");
+    printf("DEBUG_ADDRS         0x80000000\n");
+    printf("-D<debug-bits -- \n") ;
+    printf("DEBUG_SNDFD         0x00000001\n");
+    printf("DEBUG_CP (code path)0x00000002\n");
+
+} /* print_debug_bits */
+
+void print_options(void)
+{
+    printf("%s <options> [start] [stop]\n", prog_name) ;
+    printf("  -c[<kb>] print/set max message memory usage for LiS\n");
+    printf("  -C[<kb>] print/set max memory usage for LiS\n");
+    printf("  -d<msk>  debug mask (in hex)\n");
+    printf("  -D<msk>  second debug mask (in hex)\n");
+    printf("  -s       print streams memory stats\n");
+    printf("  -S       print streams queue run stats\n");
+    printf("  -m       print memory allocation to log file (from kernel)\n");
+    printf("  -p       print SPL trace buffer to log file (from kernel)\n");
+    printf("  -q       print all queues to log file (from kernel)\n");
+    printf("  -t       print streams timing stats\n");
+    printf("  start    starts the streams subsystem\n");
+    printf("  stop     stops the streams subsystem\n");
+    printf("  status   reports status of streams subsystem\n");
+
+    if (Hflag)
+	print_debug_bits() ;
+
+} /* print_options */
+
+int main( int argc, char *argv[])
+{
+    int		fd ;
+    int		rslt ;
+    int		c;
+    unsigned long debug_mask;
+    unsigned long debug_mask2;
+    extern char *optarg;
+
+    prog_name = argv[0] ;
+
+    /* 
+     * Check for mnemonic options.  These must appear alone
+     * after the command name.
+     */
+    if (argc >= 2 && argv[1] != NULL)
+    {
+	if (strcmp(argv[1], "start") == 0)
+	{
+	    exit( system("/usr/src/LiS/strms_up") );
+	}
+
+	if (strcmp(argv[1], "stop") == 0)
+	{
+	    exit( system("/usr/src/LiS/strms_down") );
+	}
+
+	if (strcmp(argv[1], "status") == 0)
+	{
+	    exit( system("/usr/src/LiS/strms_status") );
+	}
+    }
+    
+    while(( c = getopt(argc, argv, "pqsStmhHd:D:c::C::")) != -1)
+    {
+	switch (c)
+	{
+	    case 'c':
+	        cflag = 1;
+		if (optarg != NULL)
+		{
+		    if ( sscanf(optarg, "%li", &msgmemK) != 1 )
+		    {
+			printf("invalid argument for -c: \"%s\"\n", optarg);
+			exit(1);
+		    }
+		}
+		break ;
+
+	    case 'C':
+	        Cflag = 1;
+		if (optarg != NULL)
+		{
+		    if ( sscanf(optarg, "%li", &memK) != 1 )
+		    {
+			printf("invalid argument for -C: \"%s\"\n", optarg);
+			exit(1);
+		    }
+		}
+		break ;
+
+	    case 'd':
+	        dflag = 1;
+	        if ( sscanf(optarg, "%lx", &debug_mask) != 1 )
+	        {
+	            printf("need hex argument for -d\n");
+	            exit(1);
+	        }
+		break;
+
+	    case 'D':
+	        Dflag = 1;
+	        if ( sscanf(optarg, "%lx", &debug_mask2) != 1 )
+	        {
+	            printf("need hex argument for -D\n");
+	            exit(1);
+	        }
+		break;
+
+	    case 's':
+	        sflag = 1;
+	        break;
+
+	    case 'S':
+	        Sflag = 1;
+	        break;
+
+	    case 't':
+	        tflag = 1;
+	        break;
+
+	    case 'm':
+	        mflag = 1;
+	        break;
+
+	    case 'p':
+	    	pflag = 1 ;
+		break ;
+
+	    case 'q':
+	    	qflag = 1 ;
+		break ;
+
+	    case 'H':
+		Hflag = 1 ;
+	    case 'h':
+	    case '?':
+	    default:
+		print_options() ;
+		exit(1);
+	}
+    }
+
+    if (   !mflag && !dflag && !sflag && !tflag && !pflag && !qflag
+	&& !cflag && !Cflag && !Sflag && !Dflag)
+    {
+	print_options() ;
+	exit(1);
+    }
+
+    fd = open(LOOP_CLONE, 0, 0) ;
+    if (fd < 0)
+    {
+	printf(LOOP_CLONE ": %s\n", strerror(-fd)) ;
+	exit(1) ;
+    }
+
+    if ( dflag )
+    {
+	rslt = ioctl(fd, I_LIS_SDBGMSK, debug_mask) ;
+	if (rslt < 0)
+	{
+	    printf(LOOP_CLONE ": I_LIS_SDBGMSK: %s\n", strerror(-rslt)) ;
+	    exit(1) ;
+	}
+    }
+
+    if ( Dflag )
+    {
+	rslt = ioctl(fd, I_LIS_SDBGMSK2, debug_mask2) ;
+	if (rslt < 0)
+	{
+	    printf(LOOP_CLONE ": I_LIS_SDBGMSK2: %s\n", strerror(-rslt)) ;
+	    exit(1) ;
+	}
+    }
+
+    if ( sflag || tflag )
+    {
+	/* printf("sizeof strstats: %d\n", sizeof(strstats)); */
+	rslt = ioctl(fd, I_LIS_GETSTATS, strstats) ;
+	if (rslt < 0)
+	{
+	    printf(LOOP_CLONE ": I_LIS_GETSTATS: %s\n", strerror(-rslt)) ;
+	    exit(1) ;
+	}
+	LisShowStrStats();
+    }
+
+    if ( Sflag )
+    {
+	rslt = ioctl(fd, I_LIS_QRUN_STATS, &qrun_stats) ;
+	if (rslt < 0)
+	{
+	    printf(LOOP_CLONE ": I_LIS_QRUN_STATS: %s\n", strerror(-rslt)) ;
+	    exit(1) ;
+	}
+	print_qrun_stats() ;
+    }
+
+    if ( mflag )
+    {
+	rslt = ioctl(fd, I_LIS_PRNTMEM, 0) ;
+	if (rslt < 0)
+	{
+	    printf(LOOP_CLONE ": I_LIS_PRNTMEM: %s\n", strerror(-rslt)) ;
+	    exit(1) ;
+	}
+#ifdef QNX
+	printf("The memory dump is in the /usr/lib/gcom/streams.log file\n");
+#endif
+    }
+
+    if ( pflag )
+    {
+	rslt = ioctl(fd, I_LIS_PRNTSPL, 0) ;
+	if (rslt < 0)
+	{
+	    printf(LOOP_CLONE ": I_LIS_PRNTSPL: %s\n", strerror(-rslt)) ;
+	    exit(1) ;
+	}
+    }
+
+    if ( qflag )
+    {
+	rslt = ioctl(fd, I_LIS_PRNTQUEUES, 0) ;
+	if (rslt < 0)
+	{
+	    printf(LOOP_CLONE ": I_LIS_PRNTQUEUES: %s\n", strerror(-rslt)) ;
+	    exit(1) ;
+	}
+    }
+
+    if ( cflag )
+    {
+	if (msgmemK)
+	{
+	    rslt = ioctl(fd, I_LIS_SET_MAXMSGMEM, msgmemK * 1024) ;
+	    if (rslt < 0)
+	    {
+		perror("ioctl I_LIS_SET_MAXMSGMEM") ;
+		exit(1) ;
+	    }
+
+	}
+
+	rslt = ioctl(fd, I_LIS_GET_MAXMSGMEM, &msgmemK) ;
+	if (rslt < 0)
+	{
+	    perror("ioctl I_LIS_GET_MAXMSGMEM") ;
+	    exit(1) ;
+	}
+
+	if (msgmemK == 0)
+	    printf("Maximum memory to use for messages: No limit\n") ;
+	else
+	{
+	    msgmemK /= 1024 ;
+	    printf("Maximum memory to use for messages: %ldK\n", msgmemK) ;
+	}
+    }
+
+    if ( Cflag )
+    {
+	if (memK)
+	{
+	    rslt = ioctl(fd, I_LIS_SET_MAXMEM, memK * 1024) ;
+	    if (rslt < 0)
+	    {
+		perror("ioctl I_LIS_SET_MAXMEM") ;
+		exit(1) ;
+	    }
+
+	}
+
+	rslt = ioctl(fd, I_LIS_GET_MAXMEM, &memK) ;
+	if (rslt < 0)
+	{
+	    perror("ioctl I_LIS_GET_MAXMEM") ;
+	    exit(1) ;
+	}
+
+	if (memK == 0)
+	    printf("Maximum total memory to use: No limit\n") ;
+	else
+	{
+	    memK /= 1024 ;
+	    printf("Maximum total memory to use: %ldK\n", memK) ;
+	}
+    }
+
+    close(fd) ;
+
+    return 0;
+
+} /* main */
