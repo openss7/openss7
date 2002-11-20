212683452abf04767877d6730dc3a356eb74aabe
Brian Bidulock <bidulock@openss7.org>
2002-11-19 21:18:18 -0600

    Import from CVS Tue Nov 19 21:18:18 2002 -0600
    Import of LiS-2.16.0
    Initial revision

---
+++ LiS/include/sys/LiS/user-cmn.h
@@ -0,0 +1,195 @@
+/************************************************************************
+*                      User-level Common Structures                     *
+*************************************************************************
+*									*
+* The defines and data structures in this file are common to all uses	*
+* of the user-level file system.  Thus, user-mdep.h, qnx-mdep.h,	*
+* sco-mdep.h and sys54-mdep.h all include this file.			*
+*									*
+* Copyright (C) 1997  David Grothe, Gcom, Inc <dave@gcom.com>		*
+*									*
+************************************************************************/
+/*
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330, Cambridge,
+ * MA 02139, USA.
+ * 
+ */
+
+#ifndef _USER_CMN_H
+#define	_USER_CMN_H		1
+
+#ident "@(#) LiS user-cmn.h 2.10 11/19/02 21:18:18 "
+
+#include <string.h>
+#ifndef SYS_LISLOCKS_H
+#include <sys/lislocks.h>			/* for lis_semaphore_t */
+#endif
+
+#define	inode		u_inode
+#define	inode_t		u_inode_t
+#define	file		u_file
+#define	file_t		u_file_t
+#define	file_operations	u_file_operations
+
+#define REFERENCE_INODE	1	/* inode that will not be removed */
+
+typedef struct u_inode
+{
+    struct u_inode	*i_link ;		/* in case of linked list */
+    long		 i_mode ;		/* file mode */
+    port_dev_t		 i_dev ;		/* major/minor */
+    struct stdata	*i_str ;		/* ptr to stream struct */
+    lis_semaphore_t	 i_sem ;		/* inode semaphore */
+    int			 i_use ;		/* use count */
+    int			 i_inum ;		/* inode number (phoney) */
+    long		 i_ctime ;		/* status change time */
+    long		 i_mtime ;		/* modified time */
+    long		 i_atime ;		/* accessed time */
+    long		 i_ftime ;		/* creation time */
+    int			 i_flags ;		/* flags field */
+} u_inode_t ;
+
+#define i_rdev		i_dev			/* head.c uses i_rdev */
+#define i_count		i_use			/* head.c uses i_count */
+
+typedef struct u_file
+{
+    struct u_file	*f_link ;		/* in case of linked list */
+    char		*f_name ;		/* file name */
+    int			 f_flags ;		/* file open flags */
+    int			 f_count ;		/* reference count */
+    int			 f_fdnr ;		/* file number */
+    struct inode	*f_inode ;		/* inode of file */
+    struct file_operations * f_op;		/* handler routines */
+    void		*f_drvr_ptr ;		/* driver's pointer */
+    lis_semaphore_t	 f_sem ;		/* to lock file struct */
+    void		*f_ptr ;		/* ptr for user's use */
+
+} u_file_t ;
+
+#define LOCK_FILE(f)	lis_down(&(f)->f_sem)
+#define ULOCK_FILE(f)	lis_up(&(f)->f_sem)
+
+#define FILE_INODE(f)   (f)->f_inode
+#define FILE_NAME(f)    (f)->f_name
+
+#define FILE_STR(f)     (struct stdata *)(f)->f_drvr_ptr
+#define SET_FILE_STR(f,s)  (f)->f_drvr_ptr = (void *) (s)
+#define INODE_STR(i)    (i)->i_str
+#define SET_INODE_STR(i,s) (i)->i_str = (void *) (s)
+
+#define	I_COUNT(i)	((i)->i_count)
+#define	F_COUNT(f)	((f)->f_count)
+
+struct u_file_operations			/* from linux/fs.h */
+{
+	int (*lseek) (void);
+	ssize_t (*read) (struct file *, char *, size_t, loff_t *);
+	ssize_t (*write) (struct file *, const char *, size_t, loff_t *);
+	int (*readdir) (void);
+	int (*select) (void);
+	int (*ioctl) (struct inode *, struct file *, unsigned int, unsigned long);
+
+	int (*mmap) (void);
+
+	int (*open) (struct inode *, struct file *);
+	int (*release) (struct inode *, struct file *);
+
+	int (*fsync) (void);
+	int (*fasync) (void);
+	int (*check_media_change) (void);
+	int (*revalidate) (void);
+	int (*putpmsg)(struct inode *, struct file *, void *, void *, int, int);
+	int (*getpmsg)(struct inode *, struct file *, void *, void *, int *, int *, int);
+	int (*pollfd)(struct inode *, struct file *, void *);
+};
+
+/*
+ * Defines for i_mode, compliments of include/linux/stat.h
+ */
+#ifndef S_IFREG
+
+#define S_IFMT  00170000
+#define S_IFSOCK 0140000
+#define S_IFLNK	 0120000
+#define S_IFREG  0100000
+#define S_IFBLK  0060000
+#define S_IFDIR  0040000
+#define S_IFCHR  0020000
+#define S_IFIFO  0010000
+#define S_ISUID  0004000
+#define S_ISGID  0002000
+#define S_ISVTX  0001000
+
+#endif
+
+#ifndef S_ISREG
+
+#define S_ISLNK(m)	(((m) & S_IFMT) == S_IFLNK)
+#define S_ISREG(m)	(((m) & S_IFMT) == S_IFREG)
+#define S_ISDIR(m)	(((m) & S_IFMT) == S_IFDIR)
+#define S_ISCHR(m)	(((m) & S_IFMT) == S_IFCHR)
+#define S_ISBLK(m)	(((m) & S_IFMT) == S_IFBLK)
+#define S_ISFIFO(m)	(((m) & S_IFMT) == S_IFIFO)
+#define S_ISSOCK(m)	(((m) & S_IFMT) == S_IFSOCK)
+
+#endif
+
+#ifndef S_IRWXU
+
+#define S_IRWXU 00700
+#define S_IRUSR 00400
+#define S_IWUSR 00200
+#define S_IXUSR 00100
+
+#endif
+
+#ifndef S_IRWXG
+
+#define S_IRWXG 00070
+#define S_IRGRP 00040
+#define S_IWGRP 00020
+#define S_IXGRP 00010
+
+#endif
+
+#ifndef S_IRWXO
+
+#define S_IRWXO 00007
+#define S_IROTH 00004
+#define S_IWOTH 00002
+#define S_IXOTH 00001
+
+#endif
+
+#ifndef S_IRWXUGO
+
+#define S_IRWXUGO	(S_IRWXU|S_IRWXG|S_IRWXO)
+#define S_IALLUGO	(S_ISUID|S_ISGID|S_ISVTX|S_IRWXUGO)
+#define S_IRUGO		(S_IRUSR|S_IRGRP|S_IROTH)
+#define S_IWUGO		(S_IWUSR|S_IWGRP|S_IWOTH)
+#define S_IXUGO		(S_IXUSR|S_IXGRP|S_IXOTH)
+
+#endif
+
+/*
+ * Device node support
+ */
+#define	lis_mknod	user_mknod
+#define	lis_unlink	user_unlink
+
+
+#endif
---
+++ LiS/include/sys/dki.h
@@ -0,0 +1,124 @@
+/************************************************************************
+*                     SVR4 Driver-Kernel Interface			*
+*************************************************************************
+*									*
+* These are the external definitions for SVR4 compatible DKI functions.	*
+*									*
+* Author:	David Grothe <dave@gcom.com>				*
+*									*
+* Copyright (C) 1997  David Grothe, Gcom, Inc <dave@gcom.com>		*
+*									*
+************************************************************************/
+/*
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330, Cambridge,
+ * MA 02139, USA.
+ * 
+ */
+
+#ifndef	DKI_H
+#define	DKI_H		1
+
+#ident "@(#) LiS dki.h 2.8 11/19/02 21:18:02 "
+
+#ifndef _SYS_TYPES_H
+#include <sys/types.h>
+#endif
+
+
+#ifndef NULL
+#define	NULL		( (void *) 0 )
+#endif
+
+/************************************************************************
+*                           Timeouts                                    *
+*************************************************************************
+*									*
+* The function timeout is called with a pointer to a function to	*
+* invoke at a later time, an argument to be passed to the function,	*
+* and the number of ticks to wait.  'timeout' returns an integer	*
+* that serves as a "handle" for the timer.  The untimeout function	*
+* takes a timer handle as an argument and stops the timer.		*
+*									*
+* These two functions are defined in the dki.c file.  Their		*
+* implementation depends upon lis_tmout() and lis_untimout() which	*
+* are defined in either linux-mdep.c, which uses Linux timers to	*
+* implement the functions, or in the port-mdep.c file, which implements	*
+* a portable timer algorithm based on the Linux timer mechanism.	*
+*									*
+************************************************************************/
+
+#ifdef __KERNEL__
+
+typedef void	timo_fcn_t(caddr_t arg) ;
+typedef int	toid_t ;			/* SVR4 */
+typedef int	timeout_id_t ;			/* Solaris */
+
+#define	timeout(fcn,arg,ticks)	lis_timeout_fcn(fcn,arg,ticks,__FILE__, __LINE__)
+#define	untimeout		lis_untimeout
+
+extern toid_t	lis_timeout_fcn(timo_fcn_t *timo_fcn, caddr_t arg, long ticks,
+			    char *file_name, int line_nr) ;
+extern toid_t	lis_untimeout(toid_t id) ;
+
+extern void lis_terminate_dki(void);
+
+#endif				/* __KERNEL__ */
+
+
+
+#ifndef HZ
+#define	HZ	100			/* ticks per second */
+#endif
+
+/*
+ * Convert micro-seconds to proper ticks value.  The return value of
+ * this routine is suitable for use with the "timeout" function.
+ *
+ * The routine is located in osif.c.
+ */
+unsigned lis_usectohz(unsigned usec) ;
+
+/************************************************************************
+*                        Creating Nodes                                 *
+*************************************************************************
+*									*
+* The following two routines can be used to create and remove nodes	*
+* in the "/dev" directory.  These routines are used by drivers that	*
+* wish to create these nodes dynamically at driver initialization time.	*
+* They are especially useful for dynamically loaded drivers which obtain*
+* their major device number at module load time.			*
+*									*
+* These routines have the same calling sequence as their user level	*
+* versions (do "man mknod" for example).  These all return 0 upon	*
+* success and a negative error number upon failure.			*
+*									*
+* When using mknod, remember to or in S_IFCHR with the mode (<stat.h>).	*
+*									*
+************************************************************************/
+
+extern int	lis_mknod(char *name, int mode, dev_t dev) ;
+extern int	lis_unlink(char *name) ;
+extern int	lis_mount(char *dev_name,
+			  char *dir_name,
+			  char *fstype,
+			  unsigned long rwflag,
+			  void *data) ;
+extern int	lis_umount2(char *path, int flags) ;
+
+
+
+
+#endif				/* from top of file */
