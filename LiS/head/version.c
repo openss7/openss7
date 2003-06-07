62ed9b4d883efd8273c7ebb51ec319440ac36dad
Brian Bidulock <bidulock@openss7.org>
2003-06-06 22:10:29 -0600

    Import from CVS Fri Jun  6 22:10:29 2003 -0600
    Import of LiS-2.16.11

--- LiS/head/version.c
+++ LiS/head/version.c
@@ -34,8 +34,8 @@
 #ident "@(#) LiS version.c 2.16 3/10/03 17:33:40 "
 
 
-char	lis_version[] = "2.16.10" ;
-char	lis_date[]    = "6/2/03" ;
+char	lis_version[] = "2.16.11" ;
+char	lis_date[]    = "6/6/03" ;
 
 #if 0
 
