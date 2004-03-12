a7ce59aa4b42f9d6a8afadad84d646a2390ca302
Brian Bidulock <bidulock@openss7.org>
2004-03-11 20:16:14 -0600

    Import from CVS Thu Mar 11 20:16:14 2004 -0600
    -m- Import of LIS-2.18.0.

--- LiS/libc/getpmsg.c
+++ LiS/libc/getpmsg.c
@@ -1,6 +1,71 @@
 #include <sys/stropts.h>		/* must be LiS version */
 #include <unistd.h>
+#include <string.h>
 
+#ifdef BLD32OVER64
+typedef struct strbuf6 {
+    int       maxlen;
+    int       len;
+    long long buf;
+} strbuf6_t;
+
+typedef struct getpmsg_args6
+{
+    int                  fd ;
+    long long            ctl ;
+    long long            dat ;
+    long long            bandp ;
+    long long            flagsp ;
+
+} getpmsg_args6_t ;
+
+int	getpmsg(int fd, strbuf_t *ctlptr, strbuf_t *dataptr,
+		int *bandp, int *flagsp)
+{
+    strbuf6_t ctl6;
+    strbuf6_t * ptrc6 = 0;
+    strbuf6_t dat6;
+    strbuf6_t * ptrd6 = 0;
+    getpmsg_args6_t  args ;
+    int rc;
+
+    if (ctlptr)
+    {
+      ctl6.maxlen = ctlptr->maxlen;
+      ctl6.len    = ctlptr->len;
+      ctl6.buf    = (long long)ctlptr->buf;
+      ptrc6 = &ctl6;
+    }
+
+    if (dataptr)
+    {
+      dat6.maxlen = dataptr->maxlen;
+      dat6.len    = dataptr->len;
+      dat6.buf    = (long long)dataptr->buf;
+      ptrd6 = &dat6;
+    }
+
+    memset((void*)&args,0,sizeof(getpmsg_args6_t));
+    args.fd	= fd ;
+    args.ctl	= (long long)ptrc6 ;
+    args.dat	= (long long)ptrd6 ;
+    args.bandp	= (long long)bandp ;
+    args.flagsp	= (long long)flagsp ;
+
+    rc = read(fd, &args, LIS_GETMSG_PUTMSG_ULEN);
+
+    if (ctlptr)
+    {
+      ctlptr->len  = ctl6.len;
+    }
+    if (dataptr)
+    {
+      dataptr->len = dat6.len;
+    }
+
+    return(rc);
+}
+#else
 int	getpmsg(int fd, strbuf_t *ctlptr, strbuf_t *dataptr,
 		int *bandp, int *flagsp)
 {
@@ -14,3 +79,4 @@ int	getpmsg(int fd, strbuf_t *ctlptr, strbuf_t *dataptr,
 
     return(read(fd, &args, LIS_GETMSG_PUTMSG_ULEN)) ;
 }
+#endif
--- LiS/libc/putpmsg.c
+++ LiS/libc/putpmsg.c
@@ -1,6 +1,59 @@
 #include <sys/stropts.h>		/* must be LiS version */
 #include <unistd.h>
+#include <string.h>
 
+#ifdef BLD32OVER64
+typedef struct strbuf6 {
+    int       maxlen;
+    int       len;
+    long long buf;
+} strbuf6_t;
+
+typedef struct putpmsg_args6
+{
+    int                  fd ;
+    long long            ctl ;
+    long long            dat ;
+    int                  band ;
+    int                  flags ;
+
+} putpmsg_args6_t ;
+
+int	putpmsg(int fd, strbuf_t *ctlptr, strbuf_t *dataptr,
+		int band, int flags)
+{
+    putpmsg_args6_t	args ;
+    strbuf6_t ctl6;
+    strbuf6_t * ptrc6 = 0;
+    strbuf6_t dat6;
+    strbuf6_t * ptrd6 = 0;
+
+    if (ctlptr)
+    {
+      ctl6.maxlen = ctlptr->maxlen;
+      ctl6.len    = ctlptr->len;
+      ctl6.buf    = (long long)ctlptr->buf;
+      ptrc6 = &ctl6;
+    }
+
+    if (dataptr)
+    {
+      dat6.maxlen = dataptr->maxlen;
+      dat6.len    = dataptr->len;
+      dat6.buf    = (long long)dataptr->buf;
+      ptrd6 = &dat6;
+    }
+
+    memset((void*)&args,0,sizeof(putpmsg_args6_t));
+    args.fd	= fd ;
+    args.ctl	= (long long)ptrc6 ;
+    args.dat	= (long long)ptrd6 ;
+    args.band	= band ;
+    args.flags	= flags ;
+
+    return(write(fd, &args, LIS_GETMSG_PUTMSG_ULEN)) ;
+}
+#else
 int	putpmsg(int fd, strbuf_t *ctlptr, strbuf_t *dataptr,
 		int band, int flags)
 {
@@ -14,3 +67,4 @@ int	putpmsg(int fd, strbuf_t *ctlptr, strbuf_t *dataptr,
 
     return(write(fd, &args, LIS_GETMSG_PUTMSG_ULEN)) ;
 }
+#endif
