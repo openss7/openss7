3783d5c7cb4183a63715b128162ad8cc5723ccee
Brian Bidulock <bidulock@openss7.org>
2004-04-07 08:38:55 -0600

    Import from CVS Wed Apr  7 08:38:55 2004 -0600
    - Added new files.
    - Preparing for release.

---
+++ strxnet/src/include/sys/xti_osi.h
@@ -0,0 +1,53 @@
+/*****************************************************************************
+
+ @(#) $Id: xti_osi.h,v 0.9 2004/04/07 08:13:19 brian Exp $
+
+ -----------------------------------------------------------------------------
+
+     Copyright (C) 2001-2004 OpenSS7 Corporation.  All Rights Reserved.
+
+                                  PUBLIC LICENSE
+
+     This license is provided without fee, provided that the above copy-
+     right notice and this public license must be retained on all copies,
+     extracts, compilations and derivative works.  Use or distribution of
+     this work in a manner that restricts its use except as provided here
+     will render this license void.
+
+     The author(s) hereby waive any and all other restrictions in respect
+     of their copyright in this software and its associated documentation.
+     The authors(s) of this software place in the public domain any novel
+     methods or processes which are embodied in this software.
+
+     The author(s) undertook to write it for the sake of the advancement
+     of the Arts and Sciences, but it is provided as is, and the author(s)
+     will not take any responsibility in it.
+
+ -----------------------------------------------------------------------------
+
+ U.S. GOVERNMENT RESTRICTED RIGHTS.  If you are licensing this Software on
+ behalf of the U.S. Government ("Government"), the following provisions apply
+ to you.  If the Software is supplied by the Department of Defense ("DoD"), it
+ is classified as "Commercial Computer Software" under paragraph 252.227-7014
+ of the DoD Supplement to the Federal Acquisition Regulations ("DFARS") (or any
+ successor regulations) and the Government is acquiring only the license rights
+ granted herein (the license rights customarily provided to non-Government
+ users).  If the Software is supplied to any unit or agency of the Government
+ other than DoD, it is classified as "Restricted Computer Software" and the
+ Government's rights in the Software are defined in paragraph 52.227-19 of the
+ Federal Acquisition Regulations ("FAR") (or any success regulations) or, in
+ the cases of NASA, in paragraph 18.52.227-86 of the NASA Supplement to the FAR
+ (or any successor regulations).
+
+ -----------------------------------------------------------------------------
+
+ Last Modified $Date: 2004/04/07 08:13:19 $ by $Author: brian $
+
+ *****************************************************************************/
+
+#ifndef _SYS_XTI_OSI_H
+#define _SYS_XTI_OSI_H
+
+#ident "@(#) $Name:  $($Revision: 0.9 $) Copyright (c) 2001 OpenSS7 Corporation."
+
+#endif				/* _SYS_XTI_OSI_H */
