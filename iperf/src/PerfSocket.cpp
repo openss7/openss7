a121e5a94eb52b583fe1083bec1b7537dd2bef95
Brian Bidulock <bidulock@openss7.org>
2004-12-29 19:58:26 -0600

    Import from CVS Wed Dec 29 19:58:26 2004 -0600
    - Added copyright headers.
    file LICENSE was initially added on branch IPERF-2_0_1-patches.

--- iperf/src/PerfSocket.cpp
+++ iperf/src/PerfSocket.cpp
@@ -1,3 +1,60 @@
+//  ==========================================================================
+//  
+//  @(#) $Id: $
+//  
+//  --------------------------------------------------------------------------
+//  
+//  Copyright (C) 2001-2004  OpenSS7 Corporation <http://www.openss7.com>
+//  Copyright (C) 1997-2000  Brian F. G. Bidulock <bidulock@openss7.org>
+//  
+//  All Rights Reserved.
+//  
+//  This program is free software; you can redistribute it and/or modify it
+//  under the terms of the GNU General Public License as published by the Free
+//  Software Foundation; either version 2 of the License, or (at your option)
+//  any later version.
+//  
+//  This program is distributed in the hope that it will be useful, but
+//  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+//  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+//  for more details.
+//  
+//  You should have received a copy of the GNU General Public License along
+//  with this program; if not, write to the Free Software Foundation, Inc.,
+//  675 Mass Ave, Cambridge, MA 02139, USA.
+//  
+//  --------------------------------------------------------------------------
+//  
+//  U.S. GOVERNMENT RESTRICTED RIGHTS.  If you are licensing this Software on
+//  behalf of the U.S. Government ("Government"), the following provisions
+//  apply to you.  If the Software is supplied by the Department of Defense
+//  ("DoD"), it is classified as "Commercial Computer Software" under
+//  paragraph 252.227-7014 of the DoD Supplement to the Federal Acquisition
+//  Regulations ("DFARS") (or any successor regulations) and the Government is
+//  acquiring only the license rights granted herein (the license rights
+//  customarily provided to non-Government users).  If the Software is
+//  supplied to any unit or agency of the Government other than DoD, it is
+//  classified as "Restricted Computer Software" and the Government's rights
+//  in the Software are defined in paragraph 52.227-19 of the Federal
+//  Acquisition Regulations ("FAR") (or any success regulations) or, in the
+//  cases of NASA, in paragraph 18.52.227-86 of the NASA Supplement to the FAR
+//  (or any successor regulations).
+//  
+//  --------------------------------------------------------------------------
+//  
+//  Commercial licensing and support of this software is available from
+//  OpenSS7 Corporation at a fee.  See http://www.openss7.com/
+//  
+//  --------------------------------------------------------------------------
+//  
+//  Last Modified $Date: $ by $Author: $
+//  
+//  ==========================================================================
+
+#ident "@(#) $Id$"
+
+static char const ident[] = "$Id$";
+
 /*--------------------------------------------------------------- 
  * Copyright (c) 1999,2000,2001,2002,2003                              
  * The Board of Trustees of the University of Illinois            
@@ -105,9 +162,12 @@ void SetSocketOptions( thread_Settings *inSettings ) {
 #endif
 
     if ( isSCTP( inSettings ) ) {
+#if !defined SCTP_MAXSEG && defined TCP_MAXSEG
+#define SCTP_MAXSEG TCP_MAXSEG
+#endif
 #ifdef SCTP_MAXSEG
 	// set SCTP maximum segment size
-	if ( inSettingss->mMSS > 0 ) {
+	if ( inSettings->mMSS > 0 ) {
 	    int MSS = inSettings->mMSS;
 	    Socklen_t len = sizeof(MSS);
 	    int rc = setsockopt( inSettings->mSock, SOL_SCTP, SCTP_MAXSEG,
@@ -115,12 +175,15 @@ void SetSocketOptions( thread_Settings *inSettings ) {
 	    WARN_errno( rc == SOCKET_ERROR, "setsockopt SCTP_MAXSEG" );
 	}
 #endif
+#if !defined SCTP_NODELAY && defined TCP_NODELAY
+#define SCTP_NODELAY TCP_NODELAY
+#endif
 #ifdef SCTP_NODELAY
 	// set SCTP nodelay option
 	if ( isNoDelay( inSettings ) ) {
 	    int nodelay = 1;
 	    Socklen_t len = sizeof(nodelay);
-	    int rc = setsockopt( inSettings->mSock, SOL_SCTP, SCTP_NODLEAY,
+	    int rc = setsockopt( inSettings->mSock, SOL_SCTP, SCTP_NODELAY,
 				 (char *) &nodelay, len );
 	    WARN_errno( rc == SOCKET_ERROR, "setsockopt SCTP_NODELAY" );
 	}
