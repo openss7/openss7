3a9da33f2561e0518c00a5ecbc46107f458b7eef
Brian Bidulock <bidulock@openss7.org>
2005-05-26 09:59:59 -0600

    Import from CVS Thu May 26 09:59:59 2005 -0600
    - a few changes for WBEL3 release build

--- strsctp/src/drivers/sctp2.c
+++ strsctp/src/drivers/sctp2.c
@@ -1,6 +1,6 @@
 /*****************************************************************************
 
- @(#) $RCSfile: sctp2.c,v $ $Name:  $($Revision: 0.9.2.23 $) $Date: 2005/05/14 08:29:15 $
+ @(#) $RCSfile: sctp2.c,v $ $Name:  $($Revision: 0.9.2.24 $) $Date: 2005/05/26 09:59:59 $
 
  -----------------------------------------------------------------------------
 
@@ -46,21 +46,21 @@
 
  -----------------------------------------------------------------------------
 
- Last Modified $Date: 2005/05/14 08:29:15 $ by $Author: brian $
+ Last Modified $Date: 2005/05/26 09:59:59 $ by $Author: brian $
 
  *****************************************************************************/
 
-#ident "@(#) $RCSfile: sctp2.c,v $ $Name:  $($Revision: 0.9.2.23 $) $Date: 2005/05/14 08:29:15 $"
+#ident "@(#) $RCSfile: sctp2.c,v $ $Name:  $($Revision: 0.9.2.24 $) $Date: 2005/05/26 09:59:59 $"
 
 static char const ident[] =
-    "$RCSfile: sctp2.c,v $ $Name:  $($Revision: 0.9.2.23 $) $Date: 2005/05/14 08:29:15 $";
+    "$RCSfile: sctp2.c,v $ $Name:  $($Revision: 0.9.2.24 $) $Date: 2005/05/26 09:59:59 $";
 
 #include "sctp_compat.h"
 #include "sctp_hooks.h"
 
 #define SCTP_DESCRIP	"SCTP/IP STREAMS (NPI/TPI) DRIVER."
 #define SCTP_EXTRA	"Part of the OpenSS7 Stack for Linux Fast-STREAMS."
-#define SCTP_REVISION	"OpenSS7 $RCSfile: sctp2.c,v $ $Name:  $($Revision: 0.9.2.23 $) $Date: 2005/05/14 08:29:15 $"
+#define SCTP_REVISION	"OpenSS7 $RCSfile: sctp2.c,v $ $Name:  $($Revision: 0.9.2.24 $) $Date: 2005/05/26 09:59:59 $"
 #define SCTP_COPYRIGHT	"Copyright (c) 1997-2004 OpenSS7 Corporation.  All Rights Reserved."
 #define SCTP_DEVICE	"Supports Linux Fast-STREAMS and Linux NET4."
 #define SCTP_CONTACT	"Brian Bidulock <bidulock@openss7.org>"
@@ -4755,7 +4755,11 @@ sctp_queue_xmit(struct sk_buff *skb)
 {
 	struct rtable *rt = (struct rtable *) skb->dst;
 	struct iphdr *iph = skb->nh.iph;
+#ifdef NETIF_F_TSO
 	ip_select_ident_more(iph, &rt->u.dst, NULL, 0);
+#else
+	ip_select_ident(iph, &rt->u.dst, NULL);
+#endif
 	ip_send_check(iph);
 #if HAVE_KFUNC_IP_DST_OUTPUT
 	return NF_HOOK(PF_INET, NF_IP_LOCAL_OUT, skb, NULL, rt->u.dst.dev, ip_dst_output);
