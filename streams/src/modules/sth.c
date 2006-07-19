7df9a3281f644e2fb035f75914c18dd86413a334
Brian Bidulock <bidulock@openss7.org>
2006-07-18 18:07:37 -0600

    - do not ever push on write

--- streams/src/modules/sth.c
+++ streams/src/modules/sth.c
@@ -517,7 +517,7 @@ strsyscall_write(void)
 	/* NOTE:- Better peformance on both UP and SMP can be acheived by not scheduling STREAMS on
 	   the way out of a system call.  This allows queues to fill, flow control to function, and
 	   service procedures to run more efficiently. */
-#if 1
+#if 0
 	struct strthread *t = this_thread;
 
 	/* before every system call return -- saves a context switch */
