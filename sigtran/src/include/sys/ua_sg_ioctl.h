11e7e06da281895d7549ee26fb2e20edbee09afe
Brian Bidulock <bidulock@openss7.org>
2007-03-10 06:50:27 -0700

    - version number mismatches

--- sigtran/src/include/sys/ua_sg_ioctl.h
+++ sigtran/src/include/sys/ua_sg_ioctl.h
@@ -1,6 +1,6 @@
 /*****************************************************************************
 
- @(#) $Id: ua_sg_ioctl.h,v 1.1.2.1 2007/02/03 03:05:18 brian Exp $
+ @(#) $Id: ua_sg_ioctl.h,v 0.9.2.1 2007/02/03 03:05:18 brian Exp $
 
  -----------------------------------------------------------------------------
 
@@ -50,7 +50,7 @@
  -----------------------------------------------------------------------------
 
  $Log: ua_sg_ioctl.h,v $
- Revision 1.1.2.1  2007/02/03 03:05:18  brian
+ Revision 0.9.2.1  2007/02/03 03:05:18  brian
  - added new files
 
  *****************************************************************************/
@@ -58,7 +58,7 @@
 #ifndef __SYS_UA_SG_IOCTL_H__
 #define __SYS_UA_SG_IOCTL_H__
 
-#ident "@(#) $RCSfile: ua_sg_ioctl.h,v $ $Name:  $($Revision: 1.1.2.1 $) Copyright (c) 2001-2007 OpenSS7 Corporation."
+#ident "@(#) $RCSfile: ua_sg_ioctl.h,v $ $Name:  $($Revision: 0.9.2.1 $) Copyright (c) 2001-2007 OpenSS7 Corporation."
 
 /* This file can be processed by doxygen(1). */
 
