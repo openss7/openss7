6c41173d9ce8411db89a5f98af34a5df3679a44e
Brian Bidulock <bidulock@openss7.org>
2007-04-01 06:22:58 -0600

    - corrections to module loading

--- LiS/drivers/str/testmod.c
+++ LiS/drivers/str/testmod.c
@@ -1,6 +1,6 @@
 /*****************************************************************************
 
- @(#) $RCSfile: testmod.c,v $ $Name:  $($Revision: 0.9.2.9 $) $Date: 2007/03/25 18:58:11 $
+ @(#) $RCSfile: testmod.c,v $ $Name:  $($Revision: 0.9.2.10 $) $Date: 2007/04/01 12:22:00 $
 
  -----------------------------------------------------------------------------
 
@@ -45,11 +45,14 @@
 
  -----------------------------------------------------------------------------
 
- Last Modified $Date: 2007/03/25 18:58:11 $ by $Author: brian $
+ Last Modified $Date: 2007/04/01 12:22:00 $ by $Author: brian $
 
  -----------------------------------------------------------------------------
 
  $Log: testmod.c,v $
+ Revision 0.9.2.10  2007/04/01 12:22:00  brian
+ - corrections to module loading
+
  Revision 0.9.2.9  2007/03/25 18:58:11  brian
  - changes to support 2.6.20-1.2307.fc5 kernel
 
@@ -97,10 +100,10 @@
 
  *****************************************************************************/
 
-#ident "@(#) $RCSfile: testmod.c,v $ $Name:  $($Revision: 0.9.2.9 $) $Date: 2007/03/25 18:58:11 $"
+#ident "@(#) $RCSfile: testmod.c,v $ $Name:  $($Revision: 0.9.2.10 $) $Date: 2007/04/01 12:22:00 $"
 
 static char const ident[] =
-    "$RCSfile: testmod.c,v $ $Name:  $($Revision: 0.9.2.9 $) $Date: 2007/03/25 18:58:11 $";
+    "$RCSfile: testmod.c,v $ $Name:  $($Revision: 0.9.2.10 $) $Date: 2007/04/01 12:22:00 $";
 
 /*
  * This is TESTMOD a STREAMS test module that provides some specialized input-output controls meant
@@ -129,7 +132,7 @@ static char const ident[] =
 
 #define TESTMOD_DESCRIP		"UNIX SYSTEM V RELEASE 4.2 FAST STREAMS FOR LINUX"
 #define TESTMOD_COPYRIGHT	"Copyright (c) 1997-2006 OpenSS7 Corporation.  All Rights Reserved."
-#define TESTMOD_REVISION	"LfS $RCSfile: testmod.c,v $ $Name:  $($Revision: 0.9.2.9 $) $Date: 2007/03/25 18:58:11 $"
+#define TESTMOD_REVISION	"LfS $RCSfile: testmod.c,v $ $Name:  $($Revision: 0.9.2.10 $) $Date: 2007/04/01 12:22:00 $"
 #define TESTMOD_DEVICE		"SVR 4.2 Test Module for STREAMS"
 #define TESTMOD_CONTACT		"Brian Bidulock <bidulock@openss7.org>"
 #define TESTMOD_LICENSE		"GPL"
@@ -707,7 +710,6 @@ testmod_init(void)
 #ifdef LIS
 	if ((err = lis_register_module_qlock_option(modid, LIS_QLOCK_NONE)) < 0) {
 		lis_unregister_strmod(&testmod_info);
-		tm_unregister_ioctl32();
 		return (err);
 	}
 #endif
