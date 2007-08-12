5c502cd1c958c5fbb8ecb00f84ff67bfd842f236
Brian Bidulock <bidulock@openss7.org>
2007-08-12 10:40:08 -0600

    - header error

--- stacks/src/modules/sdl/spm.c
+++ stacks/src/modules/sdl/spm.c
@@ -1,6 +1,6 @@
 /*****************************************************************************
 
- @(#) $RCSfile: spm.c,v $ $Name:  $($Revision: 0.9.2.26 $) $Date: 2007/08/12 16:20:26 $
+ @(#) $RCSfile: spm.c,v $ $Name:  $($Revision: 0.9.2.27 $) $Date: 2007/08/12 16:38:45 $
 
  -----------------------------------------------------------------------------
 
@@ -45,75 +45,23 @@
 
  -----------------------------------------------------------------------------
 
- Last Modified $Date: 2007/08/12 16:20:26 $ by $Author: brian $
+ Last Modified $Date: 2007/08/12 16:38:45 $ by $Author: brian $
 
  -----------------------------------------------------------------------------
 
  $Log: spm.c,v $
+ Revision 0.9.2.27  2007/08/12 16:38:45  brian
+ - header error
+
  Revision 0.9.2.26  2007/08/12 16:20:26  brian
  - new PPA handling
 
  *****************************************************************************/
 
-#ident "@(#) $RCSfile: spm.c,v $ $Name:  $($Revision: 0.9.2.26 $) $Date: 2007/08/12 16:20:26 $"
-
-static char const ident[] = "$RCSfile: spm.c,v $ $Name:  $($Revision: 0.9.2.26 $) $Date: 2007/08/12 16:20:26 $";
-
-/*****************************************************************************
-
- @(#) $RCSfile: spm.c,v $ $Name:  $($Revision: 0.9.2.26 $) $Date: 2007/08/12 16:20:26 $
-
- -----------------------------------------------------------------------------
-
- Copyright (c) 2001-2004  OpenSS7 Corporation <http://www.openss7.com>
- Copyright (c) 1997-2000  Brian F. G. Bidulock <bidulock@openss7.org>
-
- All Rights Reserved.
-
- This program is free software; you can redistribute it and/or modify it under
- the terms of the GNU General Public License as published by the Free Software
- Foundation; version 2 of the License.
-
- This program is distributed in the hope that it will be useful, but WITHOUT
- ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
- details.
-
- You should have received a copy of the GNU General Public License along with
- this program; if not, write to the Free Software Foundation, Inc., 675 Mass
- Ave, Cambridge, MA 02139, USA.
-
- -----------------------------------------------------------------------------
-
- U.S. GOVERNMENT RESTRICTED RIGHTS.  If you are licensing this Software on
- behalf of the U.S. Government ("Government"), the following provisions apply
- to you.  If the Software is supplied by the Department of Defense ("DoD"), it
- is classified as "Commercial Computer Software" under paragraph 252.227-7014
- of the DoD Supplement to the Federal Acquisition Regulations ("DFARS") (or any
- successor regulations) and the Government is acquiring only the license rights
- granted herein (the license rights customarily provided to non-Government
- users).  If the Software is supplied to any unit or agency of the Government
- other than DoD, it is classified as "Restricted Computer Software" and the
- Government's rights in the Software are defined in paragraph 52.227-19 of the
- Federal Acquisition Regulations ("FAR") (or any successor regulations) or, in
- the cases of NASA, in paragraph 18.52.227-86 of the NASA Supplement to the FAR
- (or any successor regulations).
-
- -----------------------------------------------------------------------------
-
- Commercial licensing and support of this software is available from OpenSS7
- Corporation at a fee.  See http://www.openss7.com/
-
- -----------------------------------------------------------------------------
-
- Last Modified $Date: 2007/08/12 16:20:26 $ by $Author: brian $
-
- *****************************************************************************/
-
-#ident "@(#) $RCSfile: spm.c,v $ $Name:  $($Revision: 0.9.2.26 $) $Date: 2007/08/12 16:20:26 $"
+#ident "@(#) $RCSfile: spm.c,v $ $Name:  $($Revision: 0.9.2.27 $) $Date: 2007/08/12 16:38:45 $"
 
 static char const ident[] =
-    "$RCSfile: spm.c,v $ $Name:  $($Revision: 0.9.2.26 $) $Date: 2007/08/12 16:20:26 $";
+    "$RCSfile: spm.c,v $ $Name:  $($Revision: 0.9.2.27 $) $Date: 2007/08/12 16:38:45 $";
 
 /*
  *  This is an SDL pipemod driver for testing and use with pipes.  This module
@@ -132,7 +80,7 @@ static char const ident[] =
 #include <ss7/sdli_ioctl.h>
 
 #define SPM_DESCRIP	"SS7/SDL: (Signalling Data Terminal) STREAMS PIPE MODULE."
-#define SPM_REVISION	"OpenSS7 $RCSfile: spm.c,v $ $Name:  $($Revision: 0.9.2.26 $) $Date: 2007/08/12 16:20:26 $"
+#define SPM_REVISION	"OpenSS7 $RCSfile: spm.c,v $ $Name:  $($Revision: 0.9.2.27 $) $Date: 2007/08/12 16:38:45 $"
 #define SPM_COPYRIGHT	"Copyright (c) 1997-2002 OpenSS7 Corporation.  All Rights Reserved."
 #define SPM_DEVICE	"Provides OpenSS7 SDL pipe driver."
 #define SPM_CONTACT	"Brian Bidulock <bidulock@openss7.org>"
