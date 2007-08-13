97f72454c62b09103560f761c1951a393f0b3b96
Brian Bidulock <bidulock@openss7.org>
2007-08-13 13:11:14 -0600

    - updated headers, formatting

--- stacks/src/include/ss7/tr.h
+++ stacks/src/include/ss7/tr.h
@@ -1,6 +1,6 @@
 /*****************************************************************************
 
- @(#) $Id: tr.h,v 0.9.2.8 2007/08/03 13:35:01 brian Exp $
+ @(#) $Id: tr.h,v 0.9.2.9 2007/08/13 19:09:37 brian Exp $
 
  -----------------------------------------------------------------------------
 
@@ -11,7 +11,7 @@
 
  This program is free software; you can redistribute it and/or modify it under
  the terms of the GNU General Public License as published by the Free Software
- Foundation; version 2 of the License.
+ Foundation; version 3 of the License.
 
  This program is distributed in the hope that it will be useful, but WITHOUT
  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
@@ -19,8 +19,8 @@
  details.
 
  You should have received a copy of the GNU General Public License along with
- this program; if not, write to the Free Software Foundation, Inc., 675 Mass
- Ave, Cambridge, MA 02139, USA.
+ this program.  If not, see <http://www.gnu.org/licenses/>, or write to the
+ Free Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
  -----------------------------------------------------------------------------
 
@@ -45,11 +45,14 @@
 
  -----------------------------------------------------------------------------
 
- Last Modified $Date: 2007/08/03 13:35:01 $ by $Author: brian $
+ Last Modified $Date: 2007/08/13 19:09:37 $ by $Author: brian $
 
  -----------------------------------------------------------------------------
 
  $Log: tr.h,v $
+ Revision 0.9.2.9  2007/08/13 19:09:37  brian
+ - updated headers, formatting
+
  Revision 0.9.2.8  2007/08/03 13:35:01  brian
  - manual updates, put ss7 modules in public release
 
@@ -61,7 +64,7 @@
 #ifndef __SS7_TR_H__
 #define __SS7_TR_H__
 
-#ident "@(#) $RCSfile: tr.h,v $ $Name:  $($Revision: 0.9.2.8 $) Copyright (c) 2001-2007 OpenSS7 Corporation."
+#ident "@(#) $RCSfile: tr.h,v $ $Name:  $($Revision: 0.9.2.9 $) Copyright (c) 2001-2007 OpenSS7 Corporation."
 
 /* This file can be processed by doxygen(1). */
 
