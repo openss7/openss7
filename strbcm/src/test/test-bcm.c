385b353e513669ac25c6858c997a7df193db26cf
Brian Bidulock <bidulock@openss7.org>
2007-08-13 20:47:57 -0600

    - GPLv3 headers

--- strbcm/src/test/test-bcm.c
+++ strbcm/src/test/test-bcm.c
@@ -1,6 +1,6 @@
 /*****************************************************************************
 
- @(#) $RCSfile: test-bcm.c,v $ $Name:  $($Revision: 0.9.2.5 $) $Date: 2007/03/15 10:22:03 $
+ @(#) $RCSfile: test-bcm.c,v $ $Name:  $($Revision: 0.9.2.6 $) $Date: 2007/08/14 02:46:34 $
 
  -----------------------------------------------------------------------------
 
@@ -32,7 +32,7 @@
  -----------------------------------------------------------------------------
 
  As an exception to the above, this software may be distributed under the GNU
- General Public License (GPL) Version 2, so long as the software is distributed
+ General Public License (GPL) Version 3, so long as the software is distributed
  with, and only used for the testing of, OpenSS7 modules, drivers, and
  libraries.
 
@@ -59,11 +59,14 @@
 
  -----------------------------------------------------------------------------
 
- Last Modified $Date: 2007/03/15 10:22:03 $ by $Author: brian $
+ Last Modified $Date: 2007/08/14 02:46:34 $ by $Author: brian $
 
  -----------------------------------------------------------------------------
 
  $Log: test-bcm.c,v $
+ Revision 0.9.2.6  2007/08/14 02:46:34  brian
+ - GPLv3 headers
+
  Revision 0.9.2.5  2007/03/15 10:22:03  brian
  - test case reporting and pushed release date one day
 
@@ -81,9 +84,9 @@
 
  *****************************************************************************/
 
-#ident "@(#) $RCSfile: test-bcm.c,v $ $Name:  $($Revision: 0.9.2.5 $) $Date: 2007/03/15 10:22:03 $"
+#ident "@(#) $RCSfile: test-bcm.c,v $ $Name:  $($Revision: 0.9.2.6 $) $Date: 2007/08/14 02:46:34 $"
 
-static char const ident[] = "$RCSfile: test-bcm.c,v $ $Name:  $($Revision: 0.9.2.5 $) $Date: 2007/03/15 10:22:03 $";
+static char const ident[] = "$RCSfile: test-bcm.c,v $ $Name:  $($Revision: 0.9.2.6 $) $Date: 2007/08/14 02:46:34 $";
 
 #include <sys/types.h>
 #include <stropts.h>
@@ -120,7 +123,6 @@ static char const ident[] = "$RCSfile: test-bcm.c,v $ $Name:  $($Revision: 0.9.2
 
 #include <linux/limits.h>
 
-
 /*
  *  -------------------------------------------------------------------------
  *
@@ -135,6 +137,7 @@ static const char *lpkgname = "Linux Fast-STREAMS";
 static const char *lstdname = "UNIX SVID/ABI";
 static const char *sstdname = "SVID/ABI";
 static const char *shortname = "ABI";
+
 #ifdef LFS
 static char devname[256] = "/dev/streams/clone/bcm";
 #else
@@ -372,7 +375,7 @@ start_signals(void)
 	struct sigaction act;
 
 	act.sa_handler = signal_handler;
-//	act.sa_flags = SA_RESTART | SA_ONESHOT;
+//      act.sa_flags = SA_RESTART | SA_ONESHOT;
 	act.sa_flags = 0;
 	sigemptyset(&act.sa_mask);
 	if (sigaction(SIGALRM, &act, NULL))
@@ -1126,7 +1129,7 @@ print_pipe(int child)
 }
 
 void
-print_open(int child, const char* name)
+print_open(int child, const char *name)
 {
 	static const char *msgs[] = {
 		"open()        ----->v %-30s |  |                   \n",
@@ -1665,7 +1668,7 @@ test_insertfd(int child, int resfd, int offset, struct strbuf *ctrl, struct strb
 	fdi.flags = flags;
 	fdi.fildes = resfd;
 	fdi.offset = offset;
-	if (test_ioctl(child, I_FDINSERT, (intptr_t) & fdi) != __RESULT_SUCCESS)
+	if (test_ioctl(child, I_FDINSERT, (intptr_t) &fdi) != __RESULT_SUCCESS)
 		return __RESULT_FAILURE;
 	return __RESULT_SUCCESS;
 }
@@ -1823,7 +1826,7 @@ test_isastream(int child)
 int
 test_poll(int child, const short events, short *revents, long ms)
 {
-	struct pollfd pfd = { .fd = test_fd[child], .events = events, .revents = 0 };
+	struct pollfd pfd = {.fd = test_fd[child],.events = events,.revents = 0 };
 	int result;
 
 	print_poll(child, events);
@@ -2744,7 +2747,7 @@ ied, described, or  referred to herein.   The author  is under no  obligation to
 provide any feature listed herein.\n\
 \n\
 As an exception to the above,  this software may be  distributed  under the  GNU\n\
-General Public License (GPL) Version 2,  so long as the  software is distributed\n\
+General Public License (GPL) Version 3,  so long as the  software is distributed\n\
 with, and only used for the testing of, OpenSS7 modules, drivers, and libraries.\n\
 \n\
 U.S. GOVERNMENT RESTRICTED RIGHTS.  If you are licensing this Software on behalf\n\
@@ -2775,7 +2778,7 @@ version(int argc, char *argv[])
     %2$s\n\
     Copyright (c) 1997-2007  OpenSS7 Corporation.  All Rights Reserved.\n\
 \n\
-    Distributed by OpenSS7 Corporation under GPL Version 2,\n\
+    Distributed by OpenSS7 Corporation under GPL Version 3,\n\
     incorporated here by reference.\n\
 \n\
     See `%1$s --copying' for copying permission.\n\
