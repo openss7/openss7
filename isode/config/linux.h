9ed94cde15309c4dbec66d27e428ca07cecec707
Brian Bidulock <bidulock@openss7.org>
2007-10-20 20:46:52 -0600

    - applied patch cs.ucl.ac.uk/osimis/isode-8-patch3.linux.tar.Z

---
+++ isode/config/linux.h
@@ -0,0 +1,31 @@
+/* linux.h - site configuration file for LINUX */
+
+/*
+ *				  NOTICE
+ *
+ *    Acquisition, use, and distribution of this module and related
+ *    materials are subject to the restrictions of a license agreement.
+ *    Consult the Preface in the User's Manual for the full terms of
+ *    this agreement.
+ *
+ */
+
+
+#ifndef	_CONFIG_
+#define	_CONFIG_
+
+#define	BSD42			/* Berkeley UNIX */
+#define	SUNOS4			/*   with Sun's enhancements */
+#define	LINUX			/*   which is actually Linux */
+#define	WRITEV			/*   (sort of) real Berkeley UNIX */
+#define	BSD43			/*   4.3BSD or later */
+
+#define	VSPRINTF		/* has vprintf(3s) routines */
+
+#define	TCP			/* has TCP/IP (of course) */
+#define	SOCKETS			/*   provided by sockets */
+
+#define	NFS			/* network filesystem -- has getdirentries */
+#define TEMPNAM			/* has tempnam (3) call */
+
+#endif
