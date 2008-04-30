49f0a6560ed3fdac674cf5bc90616b24b68cf79c
Brian Bidulock <bidulock@openss7.org>
2008-04-30 08:05:19 -0600

    - updates to pass make check target

--- stacks/src/util/usage.c
+++ stacks/src/util/usage.c
@@ -1,6 +1,6 @@
 /*****************************************************************************
 
- @(#) $RCSfile: usage.c,v $ $Name:  $($Revision: 0.9.2.4 $) $Date: 2008-04-29 07:11:33 $
+ @(#) $RCSfile: usage.c,v $ $Name:  $($Revision: 0.9.2.5 $) $Date: 2008-04-30 14:05:06 $
 
  -----------------------------------------------------------------------------
 
@@ -46,11 +46,14 @@
 
  -----------------------------------------------------------------------------
 
- Last Modified $Date: 2008-04-29 07:11:33 $ by $Author: brian $
+ Last Modified $Date: 2008-04-30 14:05:06 $ by $Author: brian $
 
  -----------------------------------------------------------------------------
 
  $Log: usage.c,v $
+ Revision 0.9.2.5  2008-04-30 14:05:06  brian
+ - updates to pass make check target
+
  Revision 0.9.2.4  2008-04-29 07:11:33  brian
  - updating headers for release
 
@@ -65,9 +68,9 @@
 
  *****************************************************************************/
 
-#ident "@(#) $RCSfile: usage.c,v $ $Name:  $($Revision: 0.9.2.4 $) $Date: 2008-04-29 07:11:33 $"
+#ident "@(#) $RCSfile: usage.c,v $ $Name:  $($Revision: 0.9.2.5 $) $Date: 2008-04-30 14:05:06 $"
 
-static char const ident[] = "$RCSfile: usage.c,v $ $Name:  $($Revision: 0.9.2.4 $) $Date: 2008-04-29 07:11:33 $";
+static char const ident[] = "$RCSfile: usage.c,v $ $Name:  $($Revision: 0.9.2.5 $) $Date: 2008-04-30 14:05:06 $";
 
 #ifndef _GNU_SOURCE
 #define _GNU_SOURCE
@@ -1553,8 +1556,9 @@ ied, described, or  referred to herein.   The author  is under no  obligation to
 provide any feature listed herein.\n\
 \n\
 As an exception to the above,  this software may be  distributed  under the  GNU\n\
-General Public License (GPL) Version 3,  so long as the  software is distributed\n\
-with, and only used for the testing of, OpenSS7 modules, drivers, and libraries.\n\
+Affero  General  Public  License  (AGPL)  Version  3, so long as the software is\n\
+distributed with,  and only used for the testing of,  OpenSS7 modules,  drivers,\n\
+and libraries.\n\
 \n\
 U.S. GOVERNMENT RESTRICTED RIGHTS.  If you are licensing this Software on behalf\n\
 of the  U.S. Government  (\"Government\"),  the following provisions apply to you.\n\
