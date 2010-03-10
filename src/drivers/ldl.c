36493c99b95f2bab2844e91f0f362c818f3a6536
Brian Bidulock <bidulock@openss7.org>
2010-03-10 01:20:58 -0700

    - initial Optranex updates

--- src/drivers/ldl.c
+++ src/drivers/ldl.c
@@ -267,6 +267,22 @@ static inline void skb_set_mac_header(struct sk_buff *skb, const int offset)
  *
  *  Token ring support by Dave Grothe Copyright (C) 1999
  *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * 
+ *
  * This driver does not attempt to do any hardware programming. Instead
  * this driver is an interface between the existing Linux network device
  * driver interface and the DLPI interface.
