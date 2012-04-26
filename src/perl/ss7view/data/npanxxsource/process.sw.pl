b9ae21f2588b276c5e173361013d3e7bf6aeb5a1
Brian Bidulock <bidulock@openss7.org>
2012-04-26 08:07:47 -0600

    - switch checks

--- src/perl/ss7view/data/npanxxsource/process.sw.pl
+++ src/perl/ss7view/data/npanxxsource/process.sw.pl
@@ -493,6 +493,7 @@ close($fh);
 $fn = "$datadir/db.sw.csv";
 print STDERR "I: writing $fn\n";
 open($of,">:utf8",$fn) or die "can't write $fn";
+print $of '"',join('","',@dbsw_keys),'"',"\n";
 foreach my $npa (sort keys %dbsw) {
 	foreach my $nxx (sort keys %{$dbsw{$npa}}) {
 		foreach my $x (sort keys %{$dbsw{$npa}{$nxx}}) {
