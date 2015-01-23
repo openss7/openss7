9e14288dd8a9f40c738ca20ef91b7738b0fc8d1b
Brian Bidulock <bidulock@openss7.org>
2015-01-23 16:48:57 -0700

    removed CVS artifacts from snmp sources

--- src/snmp/mtpMIBevent.c
+++ src/snmp/mtpMIBevent.c
@@ -1,10 +1,10 @@
 /*****************************************************************************
 
- @(#) $RCSfile: mtpMIBevent.c,v $ $Name:  $($Revision: 1.1.2.2 $) $Date: 2010-11-28 14:22:14 $
+ @(#) File: src/snmp/mtpMIBevent.c
 
  -----------------------------------------------------------------------------
 
- Copyright (c) 2008-2010  Monavacon Limited <http://www.monavacon.com/>
+ Copyright (c) 2008-2015  Monavacon Limited <http://www.monavacon.com/>
  Copyright (c) 2001-2008  OpenSS7 Corporation <http://www.openss7.com/>
  Copyright (c) 1997-2001  Brian F. G. Bidulock <bidulock@openss7.org>
 
@@ -45,25 +45,9 @@
  Commercial licensing and support of this software is available from OpenSS7
  Corporation at a fee.  See http://www.openss7.com/
 
- -----------------------------------------------------------------------------
-
- Last Modified $Date: 2010-11-28 14:22:14 $ by $Author: brian $
-
- -----------------------------------------------------------------------------
-
- $Log: mtpMIBevent.c,v $
- Revision 1.1.2.2  2010-11-28 14:22:14  brian
- - remove #ident, protect _XOPEN_SOURCE
-
- Revision 1.1.2.1  2009-06-21 11:42:34  brian
- - added files to new distro
-
- Revision 0.9.2.1  2009-03-02 05:15:01  brian
- - updates
-
  *****************************************************************************/
 
-static char const ident[] = "$RCSfile: mtpMIBevent.c,v $ $Name:  $($Revision: 1.1.2.2 $) $Date: 2010-11-28 14:22:14 $";
+static char const ident[] = "File: " __FILE__ "  Version: " PACKAGE_ENVR "  Date: " PACKAGE_DATE;
 
 #include <ucd-snmp/ucd-snmp-config.h>
 #include <ucd-snmp/ucd-snmp-includes.h>
