812b64b9aacc817c4b513fa9f07a37364b2cc2cb
Brian Bidulock <bidulock@openss7.org>
2017-03-04 12:33:07 -0700

    update scripts for archlinux lts build

--- autogen.sh
+++ autogen.sh
@@ -7,7 +7,7 @@ PACKAGE=$(grep AC_INIT configure.ac|head -1|sed -r 's,AC_INIT[(][[],,;s,[]].*,,'
 GTVERSION=$(gettext --version|head -1|awk '{print$NF}'|sed -r 's,(^[^\.]*\.[^\.]*\.[^\.]*)\..*$,\1,')
 
 if [ -x "`which git 2>/dev/null`" -a -d .git ]; then
-	VERSION=$(git describe --tags|sed 's,^openss7-,,;s,\.2[0-9]*-,-,;s,[-_],.,g;s,\.g.*$,,')
+	VERSION=$(git describe --tags|sed 's,^openss7-,,;s,\.2[0-9]*-,-,;s,[-_],.,g;s,\.g.*$,,;s,\.[^.]*$,,')
 	DATE=$(git show -s --format=%ci HEAD^{commit}|awk '{print$1}')
 	sed -i.bak configure.ac -r \
 		-e "s:AC_INIT\([[]$PACKAGE[]],[[][^]]*[]]:AC_INIT([$PACKAGE],[$VERSION]:
--- rebuild.sh
+++ rebuild.sh
@@ -5,7 +5,8 @@
 rm -f cscope.*
 ./autogen.sh
 ./configure.sh
-make clean
-make V=0 cscope
-cscope -b
-make V=0 clean all README
+#make clean
+#make V=0 cscope
+#cscope -b
+#make V=0 clean all README
+make V=1
