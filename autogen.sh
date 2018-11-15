0c8ecabee66e3b13462a68aa7f8c0176e333c380
Brian Bidulock <bidulock@openss7.org>
2018-11-15 10:27:22 -0700

    make autogen handle versions better

--- autogen.sh
+++ autogen.sh
@@ -8,12 +8,17 @@ GTVERSION=$(gettext --version|head -1|awk '{print$NF}'|sed -r 's,(^[^\.]*\.[^\.]
 
 if [ -x "`which git 2>/dev/null`" -a -d .git ]; then
 	VERSION=$(git describe --tags|sed 's,^openss7-,,;s,\.2[0-9]*-,-,;s,[-_],.,g;s,\.g.*$,,;s,\.[^.]*$,,')
+	RELEASE=$(echo "$VERSION"|sed 's,.*\.,,')
+	VERSION=$(echo "$VERSION"|sed 's,\.[^.]*$,,')
 	DATE=$(git show -s --format=%ci HEAD^{commit}|awk '{print$1}')
+	PATCHLV=$(echo "$DATE"|sed 's,-,,g')
 	sed -i.bak configure.ac -r \
 		-e "s:AC_INIT\([[]$PACKAGE[]],[[][^]]*[]]:AC_INIT([$PACKAGE],[$VERSION]:
 		    s:AC_REVISION\([[][^]]*[]]\):AC_REVISION([$VERSION]):
 		    s:^DATE=.*$:DATE='$DATE':
 		    s:^AM_GNU_GETTEXT_VERSION.*:AM_GNU_GETTEXT_VERSION([$GTVERSION]):"
+	echo "$RELEASE" >.pkgrelease
+	echo "$PATCHLV" >.pkgpatchlevel
 else
 	sed -i.bak configure.ac -r \
 		-e "s:^AM_GNU_GETTEXT_VERSION.*:AM_GNU_GETTEXT_VERSION([$GTVERSION]):"
