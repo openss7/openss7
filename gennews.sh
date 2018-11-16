0cedb182fcd4dab64b52a3061c88a27560ce142d
Brian Bidulock <bidulock@openss7.org>
2018-11-16 10:41:57 -0700

    better news generating script

--- gennews.sh
+++ gennews.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # A little script to automagically generate a GNU NEWS file from git.
 
@@ -10,25 +10,50 @@ t=
 i=0
 
 for o in $(git tag --sort=-creatordate) ""; do
-	if [[ $((i++)) -ge 12 ]]; then break; fi
+	if [ $i -ge 100 ]; then break; fi
 	if [ -z "$t" ] ; then
 		head=$(git show -s --format=%H HEAD)
 		last=$(git show -s --format=%H "$o")
-		if [[ $head == $last ]]; then
+		if [ "$head" = "$last" ]; then
 			t="$o"
 			continue
 		fi
 		t="HEAD"
 		version=$(git describe --tags)
+		date=$(git show -s --format=%ci "$t^{commit}"|awk '{print$1}')
+		patchlv=$(echo "$date"|sed 's,-,,g')
 	else
 		version="$t"
+		if [ "$(echo $t|grep -q '\.20[01][0-9][0-9][0-9][0-9][0-9]$' 2>/dev/null)" != "" ] ; then
+			date=$(echo "$version"|sed -r 's,^.*\.,,;s,(....)(..)(..),\1-\2-\3,')
+			patchlv=$(echo "$date"|sed 's,-,,g')
+		else
+			date=$(git show -s --format=%ci "$t^{commit}"|awk '{print$1}')
+			patchlv=""
+		fi
 	fi
-	version=$(echo "$version"|sed 's,^[^-_]*[-_],,;s,[-_],.,g;s,\.g.*$,,')
-	date=$(git show -s --format=%ci "$t^{commit}"|awk '{print$1}')
+	i=$((i+1))
+	version=$(echo "$version"|sed 's,^openss7-,,;s,\.2[0-9]*-,-,;s,[-_],.,g;s,\.g.*$,,;s,\.[^.]*$,,')
+	version="$version${patchlv:+.$patchlv}"
 	title="Release ${PACKAGE}${PACKAGE:+-}$version released $date"
 	under=$(echo "$title"|sed 's,.,-,g')
+	/usr/bin/echo -e "\n$title\n$under\n"
+	cmd="git notes --ref=release show $t"
+	notes=$(eval $cmd 2>/dev/null|sed 's,^,    ,')
+	lines=$(echo "$notes"|wc -l)
+	if [ $lines -gt 1 ] ; then
+		/usr/bin/echo -e "$cmd\n\n$notes\n"
+	fi
+	if [ :$t != :HEAD ] ; then
+		cmd="git tag -n100 -l ${t}"
+		notes=$(eval $cmd 2>/dev/null)
+		lines=$(echo "$notes"|wc -l)
+		if [ $lines -gt 1 ] ; then
+			/usr/bin/echo -e "$cmd\n\n$notes\n"
+		fi
+	fi
 	cmd="git shortlog -e -n -w80,6,8 ${o}${o:+...}${t}"
-	echo -e "\n$title\n$under\n\n$cmd\n\n$(eval $cmd)\n"
+	/usr/bin/echo -e "$cmd\n\n$(eval $cmd)\n"
 	t="$o"
 done|sed -r 's,[[:space:]][[:space:]]*$,,'
 
