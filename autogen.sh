08ada3264062c754f779f585f7b8052145297dbc
Brian Bidulock <bidulock@openss7.org>
2019-11-04 05:25:44 -0700

    update for latest gettext

--- autogen.sh
+++ autogen.sh
@@ -4,13 +4,14 @@ cd "$(dirname "$0")"
 
 PACKAGE=$(grep AC_INIT configure.ac|head -1|sed -r 's,AC_INIT[(][[],,;s,[]].*,,')
 
-GTVERSION=$(gettext --version|head -1|awk '{print$NF}'|sed -r 's,(^[^\.]*\.[^\.]*\.[^\.]*)\..*$,\1,')
+GTVERSION=$(gettext --version|head -1|awk '{print$NF}'|sed -r 's,(^[^\.]*\.[^\.]*)\.[^\.]*$,\1,;s,(^[^\.]*\.[^\.]*\.[^\.]*)\.[^\.]*$,\1,')
 
 if [ -x "`which git 2>/dev/null`" -a -d .git ]; then
 	VERSION=$(git describe --tags|sed 's,^openss7-,,;s,\.2[0-9]*-,-,;s,[-_],.,g;s,\.g.*$,,;s,\.[^.]*$,,')
 	RELEASE=$(echo "$VERSION"|sed 's,.*\.,,')
 	VERSION=$(echo "$VERSION"|sed 's,\.[^.]*$,,')
 	DATE=$(git show -s --format=%ci HEAD^{commit}|awk '{print$1}')
+	MDOCDATE=$(perl -MDate::Format -MDate::Parse -E 'print time2str("%B %e, %Y", str2time("'"$DATE"'"))'|sed 's,  , ,')
 	BRANCH=$(git tag --sort=-creatordate|head -1)
 	GNITS="gnits "
 	if [ "$VERSION" != "$BRANCH" ]; then
@@ -18,13 +19,20 @@ if [ -x "`which git 2>/dev/null`" -a -d .git ]; then
 		GNITS=""
 	fi
 	PATCHLV=$(echo "$DATE"|sed 's,-,,g')
-	sed -i.bak configure.ac -r \
+	sed -i.bak -r \
 		-e "s:AC_INIT\([[]$PACKAGE[]],[[][^]]*[]]:AC_INIT([$PACKAGE],[$VERSION]:
 		    s:AC_REVISION\([[][^]]*[]]\):AC_REVISION([$VERSION]):
 		    s:^DATE=.*$:DATE='$DATE':
+		    s:^MDOCDATE=.*$:MDOCDATE='$MDOCDATE':
 		    s:^BRANCH=.*$:BRANCH='$BRANCH':
 		    s:^AM_GNU_GETTEXT_VERSION.*:AM_GNU_GETTEXT_VERSION([$GTVERSION]):
-		    s:^AM_INIT_AUTOMAKE\([[](gnits )?:AM_INIT_AUTOMAKE([$GNITS:"
+		    s:^AM_INIT_AUTOMAKE\([[](gnits )?:AM_INIT_AUTOMAKE([$GNITS:" \
+		configure.ac
+	subst="s:%%PACKAGE%%:$PACKAGE:g
+	       s:%%VERSION%%:$VERSION:g
+	       s:%%DATE%%:$DATE:g
+	       s:%%MDOCDATE%%:$MDOCDATE:g
+	       s:%%BRANCH%%:$BRANCH:g"
 	echo "$RELEASE" >.pkgrelease
 	echo "$PATCHLV" >.pkgpatchlevel
 else
